ce2525c5acef5ccfb1fd0a18a07b14512842d315
Pooja Khosla <pooja.khosla@canterbury.ac.nz>
2021-09-07 17:06:26 +1200

    Using docker to run python scripts and save data in database

---
+++ docker_digitaltwin/python_postgis/insert_apis_to_db.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  6 10:49:19 2021
+
+@author: pkh35
+"""
+
+
+import sqlalchemy
+import pyproj
+import setup_environment
+from api_values import api_records
+import tables
+pyproj.datadir.set_data_dir('C:\\Users\\pkh35\\Anaconda3\\envs\\digitaltwin\\Library\\share\\proj')
+pyproj.datadir.get_data_dir()
+
+def main():
+    engine = setup_environment.get_connection_from_profile(config_file_name="db_configure.yml")
+    record =api_records()
+    
+    # check if the region_geometry table exists in the database
+    insp = sqlalchemy.inspect(engine)
+    table_exist = insp.has_table("region_geometry", schema="public")
+    if table_exist == False: 
+        try:
+            response_data = tables.region_geometry(key = '197ba507238a41dea33c3be746bd9f56')
+            response_data.to_postgis('region_geometry', engine, index = True, if_exists = 'replace')
+        except Exception as error:
+            print ("An exception has occured:", error, type(error))
+    else:
+        pass
+    
+    
+    try:
+        Apilink = tables.Apilink
+        dbsession = tables.dbsession()
+        dbsession.sessionCreate(Apilink,engine)
+        
+        api_query = Apilink(source_name=record['source_name'], \
+                        source_apis=record['source_apis'], url=record['url'],\
+                        region_name=record['region_name'],api_modified_date=record['api_modified_date'],\
+                        query_dictionary="record['query_dictionary']",\
+                        geometry_col_name=record['geometry_col_name']) 
+        dbsession.runQuery(engine,api_query)
+
+        query = "UPDATE apilinks SET geometry =(SELECT geometry FROM region_geometry WHERE region_geometry.regc2021_v1_00_name = apilinks.region_name)"
+        engine.execute(query)
+        
+    except Exception as error:
+        print ("An exception has occured:", error, type(error))
+
+if __name__ == "__main__":
+    main()
+    
+
+
+
