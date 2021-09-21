c42b9b73672dacd07e1194837cc45bc275008bb6
Pooja Khosla <pooja.khosla@canterbury.ac.nz>
2021-09-21 15:41:30 +1200

    indentation check done

--- src/get_data_from_apis.py
+++ src/get_data_from_apis.py
@@ -14,55 +14,57 @@ Created on Fri Aug  6 13:26:02 2021
 
 
 import pyproj
-pyproj.datadir.set_data_dir('C:\\Users\\pkh35\\Anaconda3\\envs\\digitaltwin\\Library\\share\\proj')
+path = 'C:\\Users\\pkh35\\Anaconda3\\envs\\digitaltwin\\Library\\share\\proj'
+pyproj.datadir.set_data_dir(path)
 pyproj.datadir.get_data_dir()
-import setup_environment
+import wfs_request
 import pathlib
 import json
 import geopandas as gpd
+import setup_environment
 import srclist_check
 import polygon_Intersection as pi
 import tables
-import wfs_request
-#connect to the database where apis are stored
-engine1 = setup_environment.get_connection_from_profile(config_file_name="db_configure.yml")
 
+# connect to the database where apis are stored
+engine1 = setup_environment()
 
 # load in the instructions, get the source list and polygon from the user
-FILE_PATH = pathlib.Path().cwd() / pathlib.Path("test5.json")
+FILE_PATH = pathlib.Path().cwd() / pathlib.Path("test6.json")
 with open(FILE_PATH, 'r') as file_pointer:
-    instructions = json.load(file_pointer) 
-        
-source_list = tuple(instructions['source_name'])
+    instructions = json.load(file_pointer)
 
+source_list = tuple(instructions['source_name'])
 user_input_df = gpd.GeoDataFrame.from_features(instructions["features"])
-geometry = user_input_df.iloc[0,0]
+geometry = user_input_df.iloc[0, 0]
 
 
 User_log_info = tables.User_log_info
 dbsession = tables.dbsession()
-dbsession.sessionCreate(User_log_info,engine1)
+dbsession.sessionCreate(User_log_info, engine1)
 
-not_available_src_list, user_source_list = srclist_check.srcListCheck(engine1,source_list, str(geometry)) 
+not_available_src_list, user_source_list = srclist_check.\
+    srcListCheck(engine1, source_list, str(geometry))
 
 poly_not_available = pi.get_intersection(engine1, str(geometry))
 
-query = User_log_info(source_list=json.dumps(user_source_list, sort_keys=True, default=str), geometry=str(geometry))
-dbsession.runQuery(engine1,query)
+query = User_log_info(source_list=json.dumps(user_source_list, sort_keys=True,default=str), geometry=str(geometry))
+dbsession.runQuery(engine1, query)
 
-#take the user entered source list and polygon not available, get apis and stored data in the tables
-# get the src not available for intersecting part and get the available polygon, get apis and stored data in the tables
-if poly_not_available != None:
-    df = wfs_request.access_api_info(engine1, source_list)  #chnage it to not available sourcelist later
+"""take the user entered source list and polygon not available,
+get apis and stored data in the tables
+get the src not available for intersecting part and get the available polygon,
+get apis and stored data in the tables"""
+
+if poly_not_available is not None:
+    # chnage it to not available sourcelist later
+    df = wfs_request.access_api_info(engine1, source_list)
     key = 'e33292c76462449eb31a8c8c011c16eb'
-    for i in range(len(df)) :
-        wfs_request.data_from_apis(engine1,key,df.loc[i,'source_apis'],\
-                                                    df.loc[i,'query_dictionary'], df.loc[i, 'geometry_col_name'],\
-                                                    df.loc[i,'source_name'], poly_not_available)
+    for i in range(len(df)):
+        wfs_request.data_from_apis(engine1, key, df.loc[i, 'source_apis'],
+                                   df.loc[i, 'query_dictionary'],
+                                   df.loc[i, 'geometry_col_name'],
+                                   df.loc[i, 'source_name'], poly_not_available)
 else:
-    #get tables from the database for the AOI
+    # get tables from the database for the AOI
     pass
-
-
-
-
--- src/wfs_request.py
+++ src/wfs_request.py
@@ -10,62 +10,65 @@ import requests
 import geopandas
 import pandas as pd
 import pyproj
-pyproj.datadir.set_data_dir('C:\\Users\\pkh35\\Anaconda3\\envs\\digitaltwin\\Library\\share\\proj')
+path = 'C:\\Users\\pkh35\\Anaconda3\\envs\\digitaltwin\\Library\\share\\proj'
+pyproj.datadir.set_data_dir(path)
 pyproj.datadir.get_data_dir()
 
-def data_from_apis(engine,key,base_url,params,geometry_type,source_name, geom):
-    
-    """Construct the api query for the requested area and save it as a cache table"""
-    
-    params['cql_filter']=  f"bbox({geometry_type}, {geom.bounds[0]},\
+
+def data_from_apis(engine, key, base_url, params, geometry_type, source_name, geom):
+    """Construct the api query for the requested area
+    and save it as a cache table"""
+
+    params['cql_filter'] = f"bbox({geometry_type}, {geom.bounds[0]},\
         {geom.bounds[1]}, {geom.bounds[2]}, {geom.bounds[3]})"
     parsed_api = urlparse(base_url)
     base_url = urllib.parse.urlunparse((parsed_api.scheme, parsed_api.netloc,
-                        parsed_api.path.replace("API_KEY",key),"", "", ""))
+                                        parsed_api.path.replace
+                                        ("API_KEY", key), "",  "", ""))
     """
-    According to PsotgreSQL naming convention, a table name must start with a letter or an underscore; 
-    the rest of the string can contain letters, digits, and underscores. In our database,
-    names of the tables are based on souce_name column which in some cases starts with integer, e.g. "101292-nz-building-outlines",
+    According to PsotgreSQL naming convention,
+    a table name must start with a letter or an underscore;
+    the rest of the string can contain letters, digits, and underscores.
+    In our database,names of the tables are based on souce_name column which
+    in some cases starts with integer, e.g. "101292-nz-building-outlines",
     so "_" is appended before adding table to the database
     """
     table_name = "_"+''.join(char for char in source_name if char.isalnum())
-    
+
     try:
-        response = requests.get(base_url, params = params)
-        
+        response = requests.get(base_url, params=params)
+
     except Exception as error:
-        print (error)
-        print ("Exception TYPE:", type(error))
-        
+        print(error)
+        print("Exception TYPE:", type(error))
+
     response_data = geopandas.read_file(response.text)
-    response_data['geometry']= response_data.geometry.set_crs("EPSG:2193",allow_override=True)
-    response_data.to_postgis(table_name, engine, index = False, if_exists = 'append')
-    
-    #delete duplicate rows from the newly created tables if exists
+    response_data['geometry'] = response_data.geometry.set_crs(
+        "EPSG:2193", allow_override=True)
+
+    response_data.to_postgis(table_name, engine, index=False, if_exists='append')
+    engine.execute("update %(table_name)s set geometry =\
+                   st_flipcoordinates(geometry)", ({'table_name': table_name}))
+
+    # delete duplicate rows from the newly created tables if exists
     engine.execute("DELETE FROM %(table_name)s a USING (\
       SELECT MIN(ctid) as ctid, id\
         FROM %(table_name)s \
         GROUP BY id HAVING COUNT(*) > 1\
       ) b WHERE a.id = b.id \
-      AND a.ctid <> b.ctid" % ({'table_name':table_name}))
+      AND a.ctid <> b.ctid" % ({'table_name': table_name}))
 
 
-def access_api_info(engine, source_name): 
-    queries = engine.execute("select source_apis,query_dictionary,geometry_col_name,source_name from apilinks \
-                             where source_name IN %(source_name)s",({'source_name':source_name}))
-    api_record= []
+def access_api_info(engine, source_name):
+    queries = engine.execute("select source_apis,query_dictionary,\
+                             geometry_col_name,source_name from apilinks\
+                             where source_name IN %(source_name)s",
+                             ({'source_name': source_name}))
+    api_record = []
     for query in queries:
         api_record.append(query)
-    api_records = pd.DataFrame(api_record, columns=['source_apis','query_dictionary','geometry_col_name','source_name'])
+    api_records = pd.DataFrame(api_record, columns=['source_apis',
+                                                    'query_dictionary',
+                                                    'geometry_col_name',
+                                                    'source_name'])
     return api_records
-
-    
-    
-# import geopandas as gpd
-# from sqlalchemy import create_engine
-
-# sql = f'select * from region_geometry;'
-# eng = create_engine('postgresql+psycopg2://postgres:Pooja@localhost:5432/datasourceapis')
-
-# gdf = gpd.read_postgis(sql, eng, crs='epsg:4326')  
-# gdf.to_csv(r'P:\GRI_codes\Digital_Twin_New\src_new\region_geometry.csv')      
