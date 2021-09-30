ee5070dbdfe44dc38566b7e5afaaf06de5e8ba4d
Luke Parkinson <luke.parkinson@canterbury.ac.nz>
2021-10-01 12:43:15 +1300

    Add further detail to unit test

--- tests/test_setup_environment/test_setup_environment.py
+++ tests/test_setup_environment/test_setup_environment.py
@@ -10,12 +10,15 @@ class SetupEnvironmentTest(unittest.TestCase):
     def test_connection(self):
         """Check a connection to the database can be made with the default parameters of get_connection_from_profile"""
         engine = setup_environment.get_connection_from_profile()
-        engine.connect()
+        connection = engine.connect()
+        self.assertFalse(connection.closed,
+                         "The connection to the database failed")  # Check that the connection is open
 
     def test_incorrect_password(self):
         """Ensure that when a bad password is given to the database, the connection fails and an exception is raised"""
         incorrect_password_config_path = 'tests/test_setup_environment/mock_db_configuration.yml'
-        with self.assertRaises(OperationalError):
+        with self.assertRaises(OperationalError,
+                               msg="get_connection_from_profile should raise an OperationalError if the password supplied is incorrect"):
             setup_environment.get_connection_from_profile(incorrect_password_config_path)
 
 
