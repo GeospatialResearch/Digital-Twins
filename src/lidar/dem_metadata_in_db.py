cdd8fceec535d831b2c3048a54adc4921c8d4488
Pooja Khosla <pooja.khosla@canterbury.ac.nz>
2021-11-16 14:05:47 +1300

    store dem metadata in db

--- src/lidar/dem_metadata_in_db.py
+++ src/lidar/dem_metadata_in_db.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Nov 10 13:22:27 2021
+Created on Wed Nov 10 13:22:27 2021.
 
 @author: pkh35
 """
@@ -28,26 +28,58 @@ class DEM(Base):
     geometry = Column(Geometry('POLYGON'))
 
 
-engine = setup_environment.get_database()
-DEM.__table__.create(bind=engine, checkfirst=True)
+def check_dem_exist(instructions, engine):
+    """Only generate DEM if it doesn't exist in the database."""
+    catchment_boundary = gpd.read_file(
+        instructions['instructions']['data_paths']['catchment_boundary'])
+    geometry = str(catchment_boundary['geometry'][0])
+    query = f"select exists (Select 1 from hydrological_dem h where h.geometry ='{geometry}')"
+    row = engine.execute(query)
+    return row.fetchone()[0]
+
+
+def dem_metadata_to_db(instructions, engine):
+    """Store metadata of the generated DEM in database."""
+    DEM.__table__.create(bind=engine, checkfirst=True)
+    filepath = instructions['instructions']['data_paths']['result_dem']
+    filename = os.path.basename(filepath)
+    catchment_boundary = gpd.read_file(
+        instructions['instructions']['data_paths']['catchment_boundary'])
+    geometry = str(catchment_boundary['geometry'][0])
+    lidar = DEM(filepath=filepath, Filename=filename, geometry=geometry)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.add(lidar)
+    session.commit()
+
+
+def dem_metadata_from_db(instructions, engine):
+    """Get requested dem information from the database."""
+    catchment_boundary = gpd.read_file(
+        instructions['instructions']['data_paths']['catchment_boundary'])
+    geometry = str(catchment_boundary['geometry'][0])
+    query = f"SELECT filepath FROM hydrological_dem h where h.geometry = '{geometry}'"
+    dem = engine.execute(query)
+    return dem.fetchone()[0]
+
 
 def main():
-    with open(r'P:/Data/file.json', 'r') as file_pointer:
+    """Call the functions."""
+    with open(r'file.json', 'r') as file_pointer:
         instructions = json.load(file_pointer)
-    filepath=instructions['instructions']['data_paths']['result_dem']
-    filename=os.path.basename(filepath)
-    catchmnet_boundary = gpd.read_file(instructions['instructions']['data_paths']['catchment_boundary'])
-    geometry=str(catchmnet_boundary['geometry'][0])
-    try:
-        runner = geofabrics.processor.DemGenerator(instructions)
-        runner.run()
-        lidar = DEM(filepath=filepath, Filename=filename, geometry=geometry)
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        session.add(lidar)
-        session.commit()
-    except Exception as error:
-        print(error, type(error))
+
+    engine = setup_environment.get_database()
+    if check_dem_exist(instructions, engine) is False:
+        try:
+            runner = geofabrics.processor.DemGenerator(instructions)
+            runner.run()
+            dem_metadata_to_db(instructions, engine)
+        except Exception as error:
+            print(error, type(error))
+    else:
+        print('DEM exist in the database')
+        dem_filepath = dem_metadata_from_db(instructions, engine)
+        print(dem_filepath)
 
 
 if __name__ == '__main__':
