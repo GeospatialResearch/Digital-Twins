09dd19e4ecf24a217449ed14932e643f93026e08
Luke Parkinson <luke.parkinson@canterbury.ac.nz>
2021-12-08 14:11:40 +1300

    Add vue 2 with typescript

---
+++ visualisation/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>Hello Vue</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'App'
+})
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
