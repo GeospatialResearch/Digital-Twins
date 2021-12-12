bda87d1887e8e25ce386c1e2459dbed900f2b48b
Luke Parkinson <luke.parkinson@canterbury.ac.nz>
2021-12-13 12:44:42 +1300

    Import MapViewer from geo-visualisation-components

--- visualisation/src/App.vue
+++ visualisation/src/App.vue
@@ -1,15 +1,33 @@
 <template>
   <div id="app">
-    <h1>Hello Vue</h1>
+    <MapViewer
+      class="full-screen-map"
+      :init-lat="kaiapoi.latitude"
+      :init-long="kaiapoi.longitude"
+      :token="hereApiToken"
+    />
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from 'vue';
+import {MapViewer} from 'geo-visualisation-components/src/components';
 
 export default Vue.extend({
-  name: 'App'
-})
+  name: 'App',
+  components: {
+    MapViewer,
+  },
+  data() {
+    return {
+      kaiapoi: {
+        latitude: -43.380881,
+        longitude: 172.655714
+      },
+      hereApiToken: process.env.VUE_APP_HEREAPI,
+    }
+  },
+});
 </script>
 
 <style>
