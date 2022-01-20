47484eb902edcb94f5f6b5ddb8af32d62386ec58
Pooja Khosla <pooja.khosla@canterbury.ac.nz>
2022-01-20 18:14:06 +1300

    add comment for the variables used to create equations

--- src/dynamic_boundary_conditions/hyetograph.py
+++ src/dynamic_boundary_conditions/hyetograph.py
@@ -12,30 +12,31 @@ from src.dynamic_boundary_conditions import hirds_depth_data_from_db
 from src.dynamic_boundary_conditions import hirds_depth_data_to_db
 
 
-def hyetograph(ari, duration, site, rain_depth):
+def hyetograph(ari, duration, site, total_rain_depth):
     """Take inputs from the database to create a design storm."""
     # m and n are the ordinate of (proportion of rain fallen at) and abscissa of (propotion of time past at) the peak rainfall depth
     m = 0.53
     n = 0.55
+    #wl and wr are warp or shape factors left and right of the point of contraflexure or peak rainfall depth
     wl = 4.00
     wr = 4.61
-    R = rain_depth
+    R = total_rain_depth
     N = duration
     time_list = []
     prcp_list = []
     for t in range(N+1):
-        Dt = t/N
+        Dt = t/N  # Dt is the proportion of the duration
         if Dt <= n:
             x = (((t)/N)-n)*wl
         else:
             x = (((t)/N)-n)*wr
         tan = numpy.tanh(x)
         s = (m*tan)+m
-        Rt = R*s
-        Rt = "{:.2f}".format(Rt)
-        a, b = t,  Rt
-        time_list.append(a)
-        prcp_list.append(b)
+        Pt = R*s  # Pt is the proportion of total rainfall depth of a hyetograph,
+        Pt = "{:.2f}".format(Pt)
+        duration, proportion_rain = t, Pt
+        time_list.append(duration)
+        prcp_list.append(proportion_rain)
     df = pd.DataFrame({'time': time_list, 'prcp': prcp_list})
     del time_list, prcp_list
     df = df.astype({"time": int, "prcp": float})
@@ -46,7 +47,7 @@ def hyetograph(ari, duration, site, rain_depth):
 
 if __name__ == "__main__":
     engine = setup_environment.get_database()
-    file = 'catch4.shp'
+    file = r'P:\Data\catch5.shp'
     path = r'\\file\Research\FloodRiskResearch\DigitalTwin\hirds_depth_data'
     ari = 100
     duration = 24
