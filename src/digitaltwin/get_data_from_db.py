525d88fb340c15cc731f61f6a151755017b8fa89
Pooja Khosla <pooja.khosla@canterbury.ac.nz>
2022-01-25 10:58:46 +1300

    changed to f strings

--- src/digitaltwin/get_data_from_db.py
+++ src/digitaltwin/get_data_from_db.py
@@ -17,9 +17,11 @@ def get_data_from_db(engine, geometry, source_list):
     get_data_from_apis.get_data_from_apis(engine, geometry, source_list)
     poly = "'{}'".format(user_geometry)
     for source in source_list:
-        query = 'select * from "%(source)s" where ST_Intersects(geometry, ST_GeomFromText({}, 2193))' % (
-            {'source': source})
-        output_data = pd.read_sql_query(query.format(poly), engine)
+        query = f'select * from "{source}" where ST_Intersects(geometry, ST_GeomFromText({poly}, 2193))'
+        output_data = pd.read_sql_query(query, engine)
+        output_data['geometry'] = gpd.GeoSeries.from_wkb(output_data['geometry'])
+        output_data = gpd.GeoDataFrame(output_data, geometry='geometry')
+        print(source)
         print(output_data)
 
 
@@ -28,7 +30,7 @@ if __name__ == "__main__":
     from src.digitaltwin import setup_environment
     engine = setup_environment.get_database()
     # load in the instructions, get the source list and polygon from the user
-    FILE_PATH = pathlib.Path().cwd() / pathlib.Path("src/test1.json")
+    FILE_PATH = pathlib.Path().cwd() / pathlib.Path("src/test4.json")
     with open(FILE_PATH, 'r') as file_pointer:
         instructions = json.load(file_pointer)
     source_list = tuple(instructions['source_name'])
