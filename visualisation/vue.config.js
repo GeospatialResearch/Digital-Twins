ba4fb8bb90b4227b88e5b07b56b8e3bd3e15b937
Luke Parkinson <luke.parkinson@canterbury.ac.nz>
2022-06-21 15:29:49 +1200

    Remove cesium base url from vue.config.js

--- visualisation/vue.config.js
+++ visualisation/vue.config.js
@@ -1,7 +1,6 @@
 const cesiumSource = 'node_modules/cesium/Source';
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
-const webpack = require('webpack');
 
 module.exports = {
   configureWebpack: {
@@ -10,11 +9,7 @@ module.exports = {
         {from: path.join(cesiumSource, '../Build/Cesium/Workers'), to: 'Workers'},
         {from: path.join(cesiumSource, 'Assets'), to: 'Assets'},
         {from: path.join(cesiumSource, 'Widgets'), to: 'Widgets'}
-      ]),
-      new webpack.DefinePlugin({
-        // Define relative base path in cesium for loading assets
-        CESIUM_BASE_URL: JSON.stringify('')
-      })
+      ])
     ]
   }
 }
