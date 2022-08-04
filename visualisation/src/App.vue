29c1e501a97ca45ded8223fa53363114383c46bb
Luke Parkinson <luke.parkinson@canterbury.ac.nz>
2022-08-04 14:45:38 +1200

    Simplify way that geo-visualisation-components is imported

--- visualisation/src/App.vue
+++ visualisation/src/App.vue
@@ -10,7 +10,7 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import {MapViewer} from 'geo-visualisation-components/src/components';
+import MapViewer from 'geo-visualisation-components'
 
 export default Vue.extend({
   name: 'App',
