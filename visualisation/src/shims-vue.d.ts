f18ef8caf44e4e54ed65b139063006479f161c73
Luke Parkinson <luke.parkinson@canterbury.ac.nz>
2022-08-22 17:29:50 +1200

    Fix formatting issues

--- visualisation/src/shims-vue.d.ts
+++ visualisation/src/shims-vue.d.ts
@@ -1,4 +1,4 @@
-/**Shim to allow *.vue files to be read in TypeScript */
+/** Shim to allow *.vue files to be read in TypeScript */
 declare module '*.vue' {
   import Vue from 'vue'
   export default Vue
