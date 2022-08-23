d6afdd0c2238758c8bd3fbbc7b06a8763835074a
Luke Parkinson <luke.parkinson@canterbury.ac.nz>
2022-08-23 14:36:33 +1200

    Extract MapPage.vue and add AboutPage.vue built with bootstrap with router to switch between them

---
+++ visualisation/src/pages/MapPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- The page that shows the map for the Digital Twin -->
+  <MapViewer
+    :init-lat="kaiapoi.latitude"
+    :init-long="kaiapoi.longitude"
+    :cesium-access-token="cesiumApiToken"
+  />
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {MapViewer} from 'geo-visualisation-components/src/components';
+
+export default Vue.extend({
+  name: 'MapPage',
+  components: {
+    MapViewer,
+  },
+  data() {
+    return {
+      kaiapoi: {
+        latitude: -43.380881,
+        longitude: 172.655714
+      },
+      cesiumApiToken: process.env.VUE_APP_CESIUM_ACCESS_TOKEN,
+    }
+  },
+});
+</script>
+
+<style>
+</style>
