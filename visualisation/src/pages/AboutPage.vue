0df2e53dcbe21a16e6ae929246b11c5c98f03c5e
Luke Parkinson <luke.parkinson@canterbury.ac.nz>
2022-08-26 14:00:37 +1200

    Add license to AboutPage by reference instead of duplicating

--- visualisation/src/pages/AboutPage.vue
+++ visualisation/src/pages/AboutPage.vue
@@ -14,39 +14,29 @@
     </p>
     <hr>
     <h4>App licence</h4>
-    <p>
-      &#169; Copyright 2021 Matt Wilson/Geospatial Research Institute Toi Hangarau
-    </p>
-    <p>
-      Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
-      documentation files (the "Software"), to deal in the Software without restriction, including without limitation
-      the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
-      to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-    </p>
-    <p>
-      The above copyright notice and this permission notice shall be included in all copies or substantial portions of
-      the Software.
-    </p>
-    <p>
-      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, E
-      XPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
-      THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-      AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
-      CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-      IN THE SOFTWARE.
-    </p>
+    <div class="multiline">
+      {{ license }}
+    </div>
   </b-container>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import licenseText from "@/../../LICENSE"
 
 export default Vue.extend({
   name: "AboutPage",
   title: "About",
+  data() {
+    return {
+      license: licenseText
+    }
+  }
 });
 </script>
 
 <style scoped>
-
+div.multiline {
+  white-space: pre;
+}
 </style>
