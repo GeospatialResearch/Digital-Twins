26849c901d46ddc383d97de131022a2fc45921fa
Casey Li <casey.li@canterbury.ac.nz>
2022-09-16 11:02:39 +1200

    removed 'hyetograph' function and references to it from the 'hyetograph.py' script

--- src/dynamic_boundary_conditions/hyetograph.py
+++ src/dynamic_boundary_conditions/hyetograph.py
@@ -17,40 +17,6 @@ from src.dynamic_boundary_conditions import hirds_depth_data_to_db
 from src.dynamic_boundary_conditions import hirds_depth_data_from_db
 
 
-def hyetograph(duration, site, total_rain_depth):
-    """Take inputs from the database to create a design storm."""
-    # m is the ordinate of (proportion of rain fallen at) the peak rainfall depth
-    m = 0.53
-    # n is the abscissa of (proportion of time past at) the peak rainfall depth
-    n = 0.55
-    # wl and wr are warp or shape factors left and right of the point of contraflexure or peak rainfall depth
-    wl = 4.00
-    wr = 4.61
-    R = total_rain_depth
-    N = duration
-    time_list = []
-    prcp_list = []
-    for t in range(N + 1):
-        dt = t / N  # dt is the proportion of the duration
-        if dt <= n:
-            x = (((t) / N) - n) * wl
-        else:
-            x = (((t) / N) - n) * wr
-        tan = numpy.tanh(x)
-        s = (m * tan) + m
-        pt = R * s  # Pt is the proportion of total rainfall depth of a hyetograph
-        pt = round(pt, 2)
-        duration, proportion_rain = t, pt
-        time_list.append(duration)
-        prcp_list.append(proportion_rain)
-    hyetograph_data = pd.DataFrame({'time': time_list, 'prcp': prcp_list})
-    del time_list, prcp_list
-    hyetograph_data['prcp_prop'] = hyetograph_data['prcp'].diff()
-    # prcp_prop is the proportion of the total rainfall.
-    hyetograph_data['prcp_prop'] = hyetograph_data['prcp_prop'].fillna(0)
-    return hyetograph_data
-
-
 def catchment_area_geometry_info(catchment_file) -> shapely.geometry.Polygon:
     """Extract shapely polygon geometry from the catchment file"""
     catchment = gpd.read_file(catchment_file)
@@ -78,11 +44,6 @@ def main():
     rain_depth_in_catchment = hirds_depth_data_from_db.rain_depths_from_db(
         engine, catchment_polygon, rcp, time_period, ari, duration)
     print(rain_depth_in_catchment)
-
-    # # hyetograph data table is input into BG-Flood model
-    # for site_id, depth in zip(rain_depth_in_catchment.site_id, rain_depth_in_catchment.depth):
-    #     hyt = hyetograph(duration, site_id, depth)
-    #     hyt.plot.bar(title=f'{ari}-year storm: site {site_id}', x='time', y='prcp_prop', rot=0)
 
 
 if __name__ == "__main__":
