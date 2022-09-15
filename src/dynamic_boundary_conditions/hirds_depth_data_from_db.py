208f771162567f610066e5b5b38e82966d41e796
Casey Li <casey.li@canterbury.ac.nz>
2022-09-16 10:06:30 +1200

    incorporating 'filter_for_duration' function into the 'get_each_site_rain_depth_data' function

--- src/dynamic_boundary_conditions/hirds_depth_data_from_db.py
+++ src/dynamic_boundary_conditions/hirds_depth_data_from_db.py
@@ -39,13 +39,14 @@ def get_each_site_rain_depth_data(
             "If rcp is None, time period should be None, and vice-versa.")
         sys.exit()
     elif rcp is not None and time_period is not None:
-        query = f"""SELECT site_id, "{duration}" FROM rainfall_depth
+        query = f"""SELECT * FROM rainfall_depth
         WHERE site_id='{site_id}' AND rcp='{rcp}' AND time_period='{time_period}' AND ari={ari};"""
         rain_depth = pd.read_sql_query(query, engine)
     else:
-        query = f"""SELECT site_id, "{duration}" FROM rainfall_depth
+        query = f"""SELECT * FROM rainfall_depth
         WHERE site_id='{site_id}' AND rcp IS NULL AND time_period IS NULL AND ari={ari};"""
         rain_depth = pd.read_sql_query(query, engine).head(1)
+    rain_depth = filter_for_duration(rain_depth, duration)
     return rain_depth
 
 
