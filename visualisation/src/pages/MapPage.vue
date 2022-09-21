7861df8f0cc862fdc2e5f8bd98759165f3066464
Luke Parkinson <luke.parkinson@canterbury.ac.nz>
2022-09-21 13:45:53 +1200

    Added building and flood layers

--- visualisation/src/pages/MapPage.vue
+++ visualisation/src/pages/MapPage.vue
@@ -4,13 +4,16 @@
     :init-lat="kaiapoi.latitude"
     :init-long="kaiapoi.longitude"
     :cesium-access-token="cesiumApiToken"
+    :data-sources="dataSources"
   />
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import * as Cesium from "cesium";
 import {MapViewer} from 'geo-visualisation-components/src/components';
 import titleMixin from "@/mixins/title";
+import {MapViewerDataSourceOptions} from "geo-visualisation-components/dist/types/src/types";
 
 export default Vue.extend({
   name: "MapPage",
@@ -25,9 +28,47 @@ export default Vue.extend({
         latitude: -43.380881,
         longitude: 172.655714
       },
+      dataSources: {} as MapViewerDataSourceOptions,
       cesiumApiToken: process.env.VUE_APP_CESIUM_ACCESS_TOKEN,
     }
   },
+  created() {
+    this.loadDataSources();
+  },
+  methods: {
+    async loadDataSources() {
+      const geoJsonDataSources = await this.loadGeoJson();
+      const floodRasterAssetNumber = 946761;
+      this.dataSources = {
+        geoJsonDataSources,
+        ionAssetIds: [floodRasterAssetNumber]
+      };
+    },
+    async loadGeoJson(): Promise<Cesium.GeoJsonDataSource[]> {
+      const nonFloodBuildingDS = await Cesium.GeoJsonDataSource.load(
+        "ZS_non_flood_reproj.geojson", {
+          stroke: Cesium.Color.FORESTGREEN,
+          fill: Cesium.Color.DARKGREEN,
+          strokeWidth: 3,
+        });
+      const floodBuildingDS = await Cesium.GeoJsonDataSource.load(
+        "ZS_flooded_reproj.geojson", {
+          stroke: Cesium.Color.RED,
+          fill: Cesium.Color.DARKRED,
+          strokeWidth: 3,
+        });
+      // const allBuildings = nonFloodBuildingDS.entities.values.concat(floodBuildingDS.entities.values);
+      // for (const buildingEntity of allBuildings) {
+      //   const newHeight = 4 as unknown as Cesium.Property
+      //   let extrudedHeight = buildingEntity?.polygon?.extrudedHeight;
+      //   if (extrudedHeight != undefined) {
+      //     extrudedHeight = newHeight;
+      //   }
+      //   console.log(`Height: ${buildingEntity.polygon?.extrudedHeight}`)
+      // }
+      return [nonFloodBuildingDS, floodBuildingDS];
+    }
+  }
 });
 </script>
 
