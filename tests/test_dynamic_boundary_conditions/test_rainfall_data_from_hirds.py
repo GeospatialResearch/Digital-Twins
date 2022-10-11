943c8fed4d0aa9db562b2333ea455c775e381393
Casey Li <casey.li@canterbury.ac.nz>
2022-10-11 13:40:13 +1300

    Updated filenames

--- tests/test_dynamic_boundary_conditions/test_rainfall_data_from_hirds.py
+++ tests/test_dynamic_boundary_conditions/test_rainfall_data_from_hirds.py
@@ -8,7 +8,7 @@ from src.dynamic_boundary_conditions import rainfall_data_from_hirds
 class TestRainfallDataFromHirds(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
-        filename = "data/hirds_output.txt"
+        filename = "tests/test_dynamic_boundary_conditions/data/hirds_output.txt"
         with open(filename) as in_file:
             cls.hirds_output = in_file.read()
 
@@ -21,11 +21,13 @@ class TestRainfallDataFromHirds(unittest.TestCase):
         pass
 
     def test_get_layout_structure_of_data_just_one_block(self):
-        layout_structure = layout_structure_from_filename("data/historical_depth.txt")
+        layout_structure = layout_structure_from_filename(
+            "tests/test_dynamic_boundary_conditions/data/historical_depth.txt")
         self.assertEqual(len(layout_structure), 1)
 
     def test_get_layout_structure_of_data_rcp_nan(self):
-        layout_structure = layout_structure_from_filename("data/historical_depth.txt")
+        layout_structure = layout_structure_from_filename(
+            "tests/test_dynamic_boundary_conditions/data/historical_depth.txt")
         rcp = layout_structure[0].rcp
         self.assertTrue(math.isnan(rcp))
 
