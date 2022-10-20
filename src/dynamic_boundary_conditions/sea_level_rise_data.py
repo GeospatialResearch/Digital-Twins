55c82e1cc9e40a43f7d1f5c774e101d890622b40
Angus L <alo78@uclive.ac.nz>
2022-10-20 22:51:28 +1300

    Fixed errors and added commenting. Made inputs compatible for database.

--- src/dynamic_boundary_conditions/sea_level_rise_data.py
+++ src/dynamic_boundary_conditions/sea_level_rise_data.py
@@ -3,14 +3,24 @@
 Created on Tue Oct 18 16:30:21 2022.
 
 @author: alo78
+
+
+Used to get the sea level rise predictions (several predictions options to choose from)
+for site closest to the chosen latitude & longitude (Anywhere on the NZ Coastline) point
+from the csv's downloaded from: https://searise.takiwa.co/.
+
+
 """
 
 from datetime import date, datetime, timedelta
 import pandas as pd
 from haversine import haversine
 import os
+from shapely.geometry import Polygon
 
-def read_slr_files(path):
+
+def read_slr_files(path: str):
+    """Reads in all the SLR data csv's downloaded from https://searise.takiwa.co/ into a pandas dataframe """
     sea_level_rise_projections_nz = pd.DataFrame()
     for filename in os.listdir(path):
         if filename.endswith('.csv'):
@@ -24,23 +34,17 @@ def read_slr_files(path):
             sea_level_rise_projections_nz = pd.concat(
                 [sea_level_rise_projections_nz, sea_level_rise_projections_region], ignore_index=True, axis=0)
 
-    sea_level_rise_projections_nz.rename(columns={'siteId':
-                                                      'Site_ID',
-                                                  'year': 'Projected_Year',
-                                                  'p17': 'SLR_p17_confidence interval_meters',
-                                                  'p50': 'SLR_p50_confidence interval_meters',
-                                                  'p83': 'SLR_p83_confidence interval_meters',
-                                                  'lon': 'Longitude',
-                                                  'lat': 'Latitude',
-                                                  'measurementName': 'Scenario_(VLM with confidence level)', }, inplace=True)
+    sea_level_rise_projections_nz.rename(
+        columns={'siteId': 'Site_ID', 'year': 'Projected_Year', 'p17': 'SLR_p17_confidence interval_meters',
+                 'p50': 'SLR_p50_confidence interval_meters', 'p83': 'SLR_p83_confidence interval_meters',
+                 'lon': 'Longitude', 'lat': 'Latitude', 'measurementName': 'Scenario_(VLM with confidence level)', },
+        inplace=True)
 
     return sea_level_rise_projections_nz
 
 
-
-
-
-def unique_locations(sea_level_rise_projections_nz):
+def unique_locations(sea_level_rise_projections_nz: pd.core.frame.DataFrame):
+    """Finds the unique lat lon coordinates (site locations) and appends them to list and returns that list"""
     locations_lat_lon = []
     # Acquiring unique pairs
     unique_location = sea_level_rise_projections_nz.Location.unique().astype(str)
@@ -51,15 +55,12 @@ def unique_locations(sea_level_rise_projections_nz):
     return locations_lat_lon
 
 
+def closest_site_location(locations_lat_lon: list, user_value: dict):
+    """"Returns the haversine closest SLR site location"""
+    return min(locations_lat_lon, key=lambda p: haversine((user_value['lat'], user_value['lon']), (p['lat'], p['lon'])))
 
 
-
-def closest_site_location(data, v):
-    return min(data, key=lambda p: haversine((v['lat'], v['lon']), (p['lat'], p['lon'])))
-
-
-
-def nearest_site(locations_lat_lon, lat_input, lon_input):
+def nearest_site(locations_lat_lon: list, lat_input: float, lon_input: float):
     """ Returns lat lon coordinates which is then used by site_data function
         Selecting only the data for chosen site (e.g. 4303 is site just south
         of Waimakiriri River Mouth which is closest to a selected location of
@@ -71,26 +72,31 @@ def nearest_site(locations_lat_lon, lat_input, lon_input):
     return str(site.get("lat")) + "," + str(site.get("lon"))
 
 
-
-def site_data(sea_level_rise_projections_nz, locations_lat_lon, lat_input, lon_input):
+def site_data(sea_level_rise_projections_nz: pd.core.frame.DataFrame, locations_lat_lon: list, lat_input: float,
+              lon_input: float):
     """ Returns data for closest site by querying user for their desired location (calls nearest_site function) """
-    return sea_level_rise_projections_nz.loc[sea_level_rise_projections_nz['Location'] == nearest_site(locations_lat_lon, lat_input, lon_input)]
+    if lat_input < -90 or lat_input > 90:
+        raise ValueError("Latitude is out of range [-90, 90]")
+    if lon_input < -180 or lon_input > 180:
+        raise ValueError("Longitude is out of range [-180, 180]")
 
-
+    return sea_level_rise_projections_nz.loc[
+        sea_level_rise_projections_nz['Location'] == nearest_site(locations_lat_lon, lat_input, lon_input)]
 
 
 def main():
     path = "data/"
 
-    # Lat and Long cooords of polygon centre
-    lat_input = -43.391266
-    lon_input = 172.715979
+    ## Fetching centroid co-ordinate from user selected shapely polygon
+    lat = Polygon([[-43.298137, 172.568351], [-43.279144, 172.833569], [-43.418953, 172.826698],
+                   [-43.407542, 172.536636]]).centroid.coords[0][0]
+    long = Polygon([[-43.298137, 172.568351], [-43.279144, 172.833569], [-43.418953, 172.826698],
+                    [-43.407542, 172.536636]]).centroid.coords[0][1]
 
     nz_SLR_data = read_slr_files(path)
     unique_location_pairs = unique_locations(nz_SLR_data)
-    SLR_Site_Data = site_data(nz_SLR_data, unique_location_pairs, lat_input, lon_input)
+    SLR_Site_Data = site_data(nz_SLR_data, unique_location_pairs, lat, long)
     print(SLR_Site_Data)
-
 
 
 if __name__ == "__main__":
--- src/dynamic_boundary_conditions/tide_data.py
+++ src/dynamic_boundary_conditions/tide_data.py
@@ -3,18 +3,31 @@
 Created on Tue Oct 18 16:30:21 2022.
 
 @author: alo78
+
+
+
+Used to get the tide model predictions for site closest to the chosen latitude & longitude (Anywhere on the NZ Coastline) point
+from the API created by NIWA (https://developer.niwa.co.nz/docs/tide-api/1/overview). User can specify how many days of data is
+required and how much data (in days) either side of the highest tide value they want. They are then outputted a dataframe selection
+that includes the highest tide and either side data.
+
+
 """
 
+# Imported packages
 import os
 from dotenv import load_dotenv
 from datetime import date, datetime, timedelta
 import pandas as pd
 import requests
+from shapely.geometry import Polygon
 
 
-
-def get_Tide_data(NIWA_API_KEY, lat, long, total_days, startDate, datum):
+def get_Tide_data(NIWA_API_KEY: str, lat: float, long: float, total_days: int, startDate: str, datum: str):
     """ returns dataframe that has been concatenated with new data as max output of days data from API is 31"""
+
+    # As the API can only process max 31 days at a time, calculating the remainder and doing 1 API request
+    # Then doing 30 * the amount that the days go into 30.
     modulo = total_days % 30
     if modulo == 0:
         numberOfDays = 30
@@ -23,20 +36,21 @@ def get_Tide_data(NIWA_API_KEY, lat, long, total_days, startDate, datum):
         numberOfDays = modulo
         months = total_days // 30
 
+    # Setting API parameters
     parameters = {'lat': lat, 'long': long, 'apikey': NIWA_API_KEY, 'numberOfDays': numberOfDays,
                   'startDate': startDate, 'datum': datum}
 
     # A GET request for NIWA API
     NIWA_API = requests.get('https://api.niwa.co.nz/tides/data', params=parameters)
 
+    # Checking API request is valid and quitting if not
     if NIWA_API.status_code != 200:
-        print("API ERROR: ", NIWA_API.status_code)
-        print("Please check input and try again!")
-        quit()
+        raise ValueError(f"API ERROR: {NIWA_API.status_code} Please check input parameters and try again!")
     else:
-        Tide_df = pd.DataFrame(NIWA_API.json()['values'][2:])
-        api_check = False
+        # Fetching values from API request
+        Tide_df = pd.DataFrame(NIWA_API.json()['values'])
 
+    # Fetching new data from the API by updating paramaters and using the last start date collected plus a day to form new start date.
     count = 0
     parameters['numberOfDays'] = 30
     while count != months:
@@ -44,15 +58,14 @@ def get_Tide_data(NIWA_API_KEY, lat, long, total_days, startDate, datum):
         startDate = str((datetime.strptime(last_val[:10], '%Y-%m-%d') + timedelta(days=1)))[:10]
         parameters['startDate'] = startDate
         print("Start: ", startDate)
-
         NIWA_API = requests.get('https://api.niwa.co.nz/tides/data', params=parameters)
-        # print(NIWA_API.json()['values'])
         new_data = pd.DataFrame(NIWA_API.json()['values'])
+        # Concatenating new data to current dataframe
         Tide_df = pd.concat([Tide_df, new_data], ignore_index=True, axis=0)
         count += 1
         print(count, "/", months)
 
-    # sort tide values highest to lowest
+    # rename columns and sort tide values highest to lowest
     Tide_df.rename(columns={'time': 'Datetime_(NZST)', 'value': 'Tide_value_(m)'}, inplace=True)
     Tide_df = Tide_df.sort_values(by=['Tide_value_(m)'], ascending=False)
 
@@ -68,43 +81,43 @@ def get_Tide_data(NIWA_API_KEY, lat, long, total_days, startDate, datum):
     return Tide_df
 
 
-
-def get_highest_tide_data(Tide_df, either_side):
+def get_highest_tide_data(Tide_df: pd.core.frame.DataFrame, either_side: int):
     """ takes dataframe and finds data either side of peak is required (in days) """
+    # setting upper and lowe index
     lower_index = (Tide_df.index[0] - timedelta(days=either_side))
     upper_index = (Tide_df.index[0] + timedelta(days=either_side))
+    # Finding highest tide value and return sorted list
     highest_tide = Tide_df.loc[lower_index.strftime('%Y-%m-%d'): upper_index.strftime('%Y-%m-%d')]
     return highest_tide.sort_index().to_string()
 
 
-
-
-
-
-
-
 def main():
-
-
     load_dotenv()
     NIWA_API_KEY = os.getenv("NIWA_API_KEY")
-    lat = -30.876
-    long = -175.543
+
+    ## Fetching centroid co-ordinate from user selected shapely polygon
+    lat = Polygon([[-43.298137, 172.568351], [-43.279144, 172.833569], [-43.418953, 172.826698],
+                   [-43.407542, 172.536636]]).centroid.coords[0][0]
+    long = Polygon([[-43.298137, 172.568351], [-43.279144, 172.833569], [-43.418953, 172.826698],
+                    [-43.407542, 172.536636]]).centroid.coords[0][1]
+
+    # How many days of tide data to collect (e.g. 365 for 1 years worth):
     total_days = 365
+    # Start date (can be in the past or present) for collection of data
+
+    # String for start date of data in format of ('yyyy-mm-dd')
     startDate = date.today()
+
+    # String for datum type.  LAT: Lowest astronomical tide; MSL: Mean sea level
     datum = "LAT"
 
+    # How many days either side of highest tide do you want data for:
     either_side = 1
 
+    Tide_dataframe = get_Tide_data(NIWA_API_KEY, lat, long, total_days, startDate, datum)
 
-
-
-    Tide_df = get_Tide_data(NIWA_API_KEY, lat, long, total_days, startDate, datum)
-
-    highest_tide_data = get_highest_tide_data(Tide_df, either_side)
+    highest_tide_data = get_highest_tide_data(Tide_dataframe, either_side)
     print(highest_tide_data)
-
-
 
 
 if __name__ == "__main__":
