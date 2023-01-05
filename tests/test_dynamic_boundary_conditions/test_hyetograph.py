b75aa14f7e9128b913098fc38ba4f251c0ed1ca4
Casey Li <casey.li@canterbury.ac.nz>
2023-01-05 13:41:35 +1300

    add and use selected_polygon.geojson for tests instead of using shapefile

--- tests/test_dynamic_boundary_conditions/test_hyetograph.py
+++ tests/test_dynamic_boundary_conditions/test_hyetograph.py
@@ -9,7 +9,7 @@ class HyetographTest(unittest.TestCase):
 
     def test_catchment_area_geometry_info_correct_type(self):
         """Test to ensure that a shapely geometry polygon is extracted from the catchment file."""
-        catchment_file_path = pathlib.Path(r"tests/test_dynamic_boundary_conditions/data/catchment_polygon.shp")
+        catchment_file_path = pathlib.Path(r"tests/test_dynamic_boundary_conditions/data/selected_polygon.geojson")
         catchment_polygon = main_rainfall.catchment_area_geometry_info(catchment_file_path)
         self.assertIsInstance(catchment_polygon, Polygon)
 
