491e8be352d96c22b944a6480bfbf3806342b2f8
Luke Parkinson <luke.parkinson@canterbury.ac.nz>
2023-01-09 11:04:59 +1300

    Update test_setup_environment.py to pass on local windows dev machines and not run in docker.

--- tests/test_digitaltwin/test_setup_environment.py
+++ tests/test_digitaltwin/test_setup_environment.py
@@ -1,6 +1,8 @@
 import os
+import platform
 import unittest
 
+import pytest
 from sqlalchemy.exc import OperationalError
 
 from src.digitaltwin import setup_environment
@@ -8,21 +10,29 @@ from src.digitaltwin import setup_environment
 
 class SetupEnvironmentTest(unittest.TestCase):
 
-    @unittest.skip("Skipping until we work on https://github.com/GeospatialResearch/Digital-Twins/issues/23")
+    @pytest.mark.skipif(
+        platform.system() != "Windows",
+        reason="This test only runs on local dev machines with a database running. "
+               "It is not set up to integrate into a test suite.")
     def test_connection(self):
         """Check a connection to the database can be made with the default parameters of get_connection_from_profile"""
         engine = setup_environment.get_connection_from_profile()
         connection = engine.connect()
         self.assertFalse(connection.closed,
-                         msg="The connection to the database failed")  # Check that the connection is open
+                         msg="The connection to the database failed and is needed for these tests.")
 
-    @unittest.skip("Skipping until we work on https://github.com/GeospatialResearch/Digital-Twins/issues/23")
+    @pytest.mark.skipif(
+        platform.system() != "Windows",
+        reason="This test only runs on local dev machines with a database running. "
+               "It is not set up to integrate into a test suite.")
     def test_incorrect_password(self):
         """Ensure that when a bad password is given to the database, the connection fails and an exception is raised"""
         # Override the variables supplied by the .env file with an incorrect password
+        self.test_connection()  # This test case requires an active connection to trust the result.
         os.environ["POSTGRES_PASSWORD"] = "incorrect_password"
         with self.assertRaises(OperationalError,
-                               msg="get_connection_from_profile should raise an OperationalError if the password supplied is incorrect"):
+                               msg="get_connection_from_profile should raise an OperationalError"
+                                   " if the password supplied is incorrect"):
             setup_environment.get_connection_from_profile()
 
 
