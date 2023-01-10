5ef4f79ef3cede009ee854792b3018be1b39fd4f
Casey Li <casey.li@canterbury.ac.nz>
2023-01-11 10:13:22 +1300

    updated get_interpolated_data() function

--- src/dynamic_boundary_conditions/hyetograph.py
+++ src/dynamic_boundary_conditions/hyetograph.py
@@ -72,12 +72,12 @@ def get_interpolated_data(
         Temporal interpolation method to be used. Refer to 'scipy.interpolate.interp1d()' for available methods.
         One of 'linear', 'nearest', 'nearest-up', 'zero', 'slinear', 'quadratic', 'cubic', 'previous', or 'next'.
     """
-    duration = np.array(transposed_catchment_data['duration_mins'])
-    if increment_mins < duration[0] or increment_mins > duration[-1]:
+    duration = transposed_catchment_data['duration_mins']
+    if increment_mins < duration.iloc[0] or increment_mins > duration.iloc[-1]:
         raise ValueError(f"Increment minute {increment_mins} is out of range, "
-                         f"needs to be between {duration[0]} and {duration[-1]}.")
-    duration_new = np.arange(increment_mins, duration[-1] + increment_mins, increment_mins)
-    duration_new = duration_new[:-1] if duration_new[-1] > duration[-1] else duration_new
+                         f"needs to be between {duration.iloc[0]} and {duration.iloc[-1]}.")
+    duration_new = np.arange(increment_mins, duration.iloc[-1] + increment_mins, increment_mins)
+    duration_new = duration_new[:-1] if duration_new[-1] > duration.iloc[-1] else duration_new
     interp_catchment_data = pd.DataFrame(duration_new, columns=["duration_mins"])
     for column_num in range(1, len(transposed_catchment_data.columns)):
         depth = transposed_catchment_data.iloc[:, column_num]
