be152233ce1381ff64a7b12df428676d4eb8afca
Luke Parkinson <luke.parkinson@canterbury.ac.nz>
2023-03-03 13:40:40 +1300

    Add failure state to tasks when exception is raised.

--- src/tasks.py
+++ src/tasks.py
@@ -1,4 +1,4 @@
-from celery import Celery
+from celery import Celery, states
 
 from .digitaltwin import run
 
@@ -7,11 +7,18 @@ message_broker_url = "redis://message_broker:6379/0"
 app = Celery("tasks", backend=message_broker_url, broker=message_broker_url)
 
 
-@app.task()
+class OnFailureStateTask(app.Task):
+    """Task that switches state to FAILURE if an exception occurs"""
+    def on_failure(self, _exc, _task_id, _args, _kwargs, _einfo):
+        self.update_state(state=states.FAILURE)
+
+
+@app.task(base=OnFailureStateTask)
 def create_model_for_area():
+    """Creates a model for the area"""
     initialise_db_with_region_geometries()
 
 
-@app.task(ignore_result=True)
+@app.task(base=OnFailureStateTask, ignore_result=True)
 def initialise_db_with_region_geometries():
     run.main()
