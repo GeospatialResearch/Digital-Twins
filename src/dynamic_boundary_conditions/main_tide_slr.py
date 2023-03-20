a6ae8523627628dded02792a9046d0419ad98d7e
Casey Li <casey.li@canterbury.ac.nz>
2023-03-20 15:05:03 +1300

    split 'get_catchment_centroid_coords' function out to keep consistency

--- src/dynamic_boundary_conditions/main_tide_slr.py
+++ src/dynamic_boundary_conditions/main_tide_slr.py
@@ -4,10 +4,41 @@
 @Author: sli229
 """
 
+import logging
 import pathlib
 from typing import Tuple
+import sqlalchemy
+from shapely.geometry import Polygon
 
 import geopandas as gpd
+import geoapis.vector
+
+from src import config
+from src.digitaltwin import setup_environment
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter("%(levelname)s:%(asctime)s:%(name)s:%(message)s")
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+
+log.addHandler(stream_handler)
+
+
+def catchment_area_geometry_info(catchment_file: pathlib.Path) -> Polygon:
+    """
+    Extract shapely geometry polygon from the catchment file.
+
+    Parameters
+    ----------
+    catchment_file
+        The file path of the catchment polygon shapefile.
+    """
+    catchment = gpd.read_file(catchment_file)
+    catchment = catchment.to_crs(4326)
+    catchment_polygon = catchment["geometry"][0]
+    return catchment_polygon
 
 
 def get_catchment_centroid_coords(catchment_file: pathlib.Path) -> Tuple[float, float]:
@@ -19,8 +50,6 @@ def get_catchment_centroid_coords(catchment_file: pathlib.Path) -> Tuple[float, 
     catchment_file : pathlib.Path
         The file path for the catchment polygon.
     """
-    catchment = gpd.read_file(catchment_file)
-    catchment = catchment.to_crs(4326)
-    catchment_polygon = catchment["geometry"][0]
+    catchment_polygon = catchment_area_geometry_info(catchment_file)
     long, lat = catchment_polygon.centroid.coords[0]
     return lat, long
