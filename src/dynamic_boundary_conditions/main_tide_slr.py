4019b75ae0479dffe11d374ada39ed00d17ea3b3
Casey Li <casey.li@canterbury.ac.nz>
2023-03-30 09:55:10 +1300

    added main_tide_slr  script

---
+++ src/dynamic_boundary_conditions/main_tide_slr.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+@Description:
+@Author: sli229
+"""
+
+import logging
+import pathlib
+from datetime import date
+
+from src import config
+from src.digitaltwin import setup_environment
+from src.dynamic_boundary_conditions.tide_enum import DatumType, ApproachType
+from src.dynamic_boundary_conditions import tide_query_location, tide_data_from_niwa, sea_level_rise_data
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter("%(levelname)s:%(asctime)s:%(name)s:%(message)s")
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+
+log.addHandler(stream_handler)
+
+
+def main():
+    # Get StatsNZ and NIWA api key
+    stats_nz_api_key = config.get_env_variable("StatsNZ_API_KEY")
+    niwa_api_key = config.get_env_variable("NIWA_API_KEY")
+    # Connect to the database
+    engine = setup_environment.get_database()
+    tide_query_location.write_nz_bbox_to_file(engine)
+    # Catchment polygon
+    catchment_file = pathlib.Path(r"selected_polygon.geojson")
+    catchment_area = tide_query_location.get_catchment_area(catchment_file)
+    # Store regional council clipped data in the database
+    tide_query_location.regional_council_clipped_to_db(engine, stats_nz_api_key, 111181)
+    # Get regions (clipped) that intersect with the catchment area from the database
+    regions_clipped = tide_query_location.get_regions_clipped_from_db(engine, catchment_area)
+    tide_query_loc = tide_query_location.get_tide_query_locations(
+        engine, catchment_area, regions_clipped, distance_km=1)
+    # Specify the datum query parameter
+    datum = DatumType.LAT
+    # Get tide data
+    tide_data = tide_data_from_niwa.get_tide_data(
+        approach=ApproachType.KING_TIDE,
+        api_key=niwa_api_key,
+        datum=datum,
+        tide_query_loc=tide_query_loc,
+        start_date=date(2023, 1, 23),
+        total_days=3,  # used for PERIOD_TIDE
+        tide_length_mins=2880,  # used for KING_TIDE
+        interval=10)
+    # Store sea level rise data to database and get closest sea level rise site data from database
+    sea_level_rise_data.store_slr_data_to_db(engine)
+    slr_data = sea_level_rise_data.get_closest_slr_data(engine, tide_data)
+    print(slr_data)
+
+
+if __name__ == "__main__":
+    main()
