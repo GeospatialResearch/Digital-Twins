2b115e0fb412dbecc751bc96ed0a9b38e88cf159
Casey Li <casey.li@canterbury.ac.nz>
2023-06-03 20:12:29 +1200

    refactor and fix dem_metadata_in_db.py

--- src/lidar/dem_metadata_in_db.py
+++ src/lidar/dem_metadata_in_db.py
@@ -2,18 +2,31 @@
 """
 Created on Wed Nov 10 13:22:27 2021.
 
-@author: pkh35
+@author: pkh35, sli229
 """
 
+import logging
 import pathlib
+from datetime import datetime
+from typing import Tuple, Dict, Any
 
 import geopandas as gpd
 from geoalchemy2 import Geometry
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.engine import Engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 
 import geofabrics.processor
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter("%(levelname)s:%(asctime)s:%(name)s:%(message)s")
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+
+log.addHandler(stream_handler)
 
 Base = declarative_base()
 
@@ -22,28 +35,52 @@ class HydroDEM(Base):
     """Class used to create 'hydrological_dem' table."""
     __tablename__ = "hydrological_dem"
     unique_id = Column(Integer, primary_key=True, autoincrement=True)
-    file_path = Column(String)
     file_name = Column(String)
+    file_path = Column(String)
+    created_at = Column(DateTime(timezone=True), default=datetime.now(), comment="output created datetime")
     geometry = Column(Geometry("GEOMETRY", srid=2193), comment="catchment area coverage")
 
 
-def dem_table(engine):
-    """Create hydrological_dem table if doesn't exists."""
-    DEM.__table__.create(bind=engine, checkfirst=True)
+def create_hydro_dem_table(engine: Engine) -> None:
+    """Create 'hydrological_dem' table in the database if it doesn't exist."""
+    HydroDEM.__table__.create(bind=engine, checkfirst=True)
 
 
-def check_dem_exist(selected_polygon: gpd.GeoDataFrame, engine):
-    """Only generate DEM if it doesn't exist in the database."""
-    dem_table(engine)
-    geometry = str(selected_polygon["geometry"][0])
-    query = (
-        f"select exists (Select 1 from hydrological_dem where geometry ='{geometry}')"
-    )
-    row = engine.execute(query)
-    return row.fetchone()[0]
+def get_hydro_dem_metadata(
+        instructions: Dict[str, Any],
+        catchment_boundary: gpd.GeoDataFrame) -> Tuple[str, str, str]:
+    """Get the hydrological DEM metadat~a."""
+    data_paths: Dict[str, Any] = instructions["instructions"]["data_paths"]
+    result_dem_path = pathlib.Path(data_paths["local_cache"]) / data_paths["subfolder"] / data_paths["result_dem"]
+    hydro_dem_name = result_dem_path.name
+    hydro_dem_path = result_dem_path.as_posix()
+    geometry = catchment_boundary["geometry"].to_wkt().iloc[0]
+    return hydro_dem_name, hydro_dem_path, geometry
 
 
-def generate_dem(instructions):
+def store_hydro_dem_metadata_to_db(
+        engine: Engine,
+        instructions: Dict[str, Any],
+        catchment_boundary: gpd.GeoDataFrame) -> None:
+    """Store metadata of the hydrologically conditioned DEM in the database."""
+    create_hydro_dem_table(engine)
+    hydro_dem_name, hydro_dem_path, geometry = get_hydro_dem_metadata(instructions, catchment_boundary)
+    with Session(engine) as session:
+        hydro_dem = HydroDEM(file_name=hydro_dem_name, file_path=hydro_dem_path, geometry=geometry)
+        session.add(hydro_dem)
+        session.commit()
+        log.info("Hydro DEM metadata stored successfully in the database.")
+
+
+def check_hydro_dem_exist(engine: Engine, catchment_boundary: gpd.GeoDataFrame) -> bool:
+    """Check if hydro DEM already exists in the database for the catchment area."""
+    create_hydro_dem_table(engine)
+    geometry = catchment_boundary["geometry"].to_wkt().iloc[0]
+    query = f"SELECT EXISTS (SELECT 1 FROM hydrological_dem WHERE geometry = '{geometry}');"
+    return engine.execute(query).scalar()
+
+
+def run_geofabrics_hydro_dem(instructions: Dict[str, Any]) -> None:
     """Use geofabrics to generate the hydrologically conditioned DEM."""
     runner = geofabrics.processor.RawLidarDemGenerator(instructions["instructions"])
     runner.run()
@@ -51,22 +88,7 @@ def generate_dem(instructions):
     runner.run()
 
 
-def dem_metadata_to_db(instructions, selected_polygon: gpd.GeoDataFrame, engine):
-    """Store metadata of the generated DEM in database."""
-    geometry = str(selected_polygon["geometry"][0])
-    data_paths = instructions["instructions"]["data_paths"]
-    cache_path = pathlib.Path(data_paths["local_cache"])
-    subfolder = data_paths["subfolder"]
-    result_dem_name = data_paths["result_dem"]
-    result_dem_path = (cache_path / subfolder / result_dem_name).as_posix()
-    lidar = HydroDEM(filepath=result_dem_path, Filename=result_dem_name, geometry=geometry)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    session.add(lidar)
-    session.commit()
-
-
-def dem_metadata_from_db(selected_polygon: gpd.GeoDataFrame, engine):
+def dem_metadata_from_db(selected_polygon: gpd.GeoDataFrame, engine: Engine):
     """Get requested dem information from the database."""
     geometry = str(selected_polygon["geometry"][0])
     query = f"SELECT filepath FROM hydrological_dem WHERE geometry = '{geometry}'"
@@ -74,10 +96,10 @@ def dem_metadata_from_db(selected_polygon: gpd.GeoDataFrame, engine):
     return dem.fetchone()[0]
 
 
-def get_dem_path(instructions, selected_polygon: gpd.GeoDataFrame, engine):
+def get_dem_path(instructions, catchment_boundary: gpd.GeoDataFrame, engine):
     """Pass dem information to other functions."""
-    if not check_dem_exist(selected_polygon, engine):
-        generate_dem(instructions)
-        dem_metadata_to_db(instructions, selected_polygon, engine)
-    dem_filepath = dem_metadata_from_db(selected_polygon, engine)
+    if not check_hydro_dem_exist(engine, catchment_boundary):
+        run_geofabrics_hydro_dem(instructions)
+        store_hydro_dem_metadata_to_db(engine, instructions, catchment_boundary)
+    dem_filepath = dem_metadata_from_db(catchment_boundary, engine)
     return dem_filepath
