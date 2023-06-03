4496465384ae67491f8ad3e3f3e0b017346a5034
Casey Li <casey.li@canterbury.ac.nz>
2023-06-04 03:53:22 +1200

    update setup_environment.py

--- src/digitaltwin/setup_environment.py
+++ src/digitaltwin/setup_environment.py
@@ -8,11 +8,10 @@ Created on Thu Aug  5 17:09:13 2021.
 import logging
 
 from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
 from sqlalchemy.ext.declarative import declarative_base
 
 from src import config
-
-Base = declarative_base()
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
@@ -23,15 +22,17 @@ stream_handler.setFormatter(formatter)
 
 log.addHandler(stream_handler)
 
+Base = declarative_base()
 
-def get_database():
+
+def get_database() -> Engine:
     """Exit the program if connection fails."""
     engine = get_connection_from_profile()
     log.debug("Connected to PostgreSQL database!")
     return engine
 
 
-def get_connection_from_profile():
+def get_connection_from_profile() -> Engine:
     """Sets up database connection from config file."""
     connection_keys = ["POSTGRES_HOST", "POSTGRES_PORT", "POSTGRES_DB", "POSTGRES_USER", "POSTGRES_PASSWORD"]
     host, port, db, username, password = (config.get_env_variable(key) for key in connection_keys)
@@ -39,20 +40,20 @@ def get_connection_from_profile():
     if any(connection_cred is None for connection_cred in [host, port, db, username, password]):
         raise ConnectionAbortedError(f"Bad .env file. Not all f{connection_keys} set.")
 
-    return get_engine(db, username, host, port, password)
+    return get_engine(host, port, db, username, password)
 
 
-def get_engine(db: str, user: str, host: str, port: str, password: str):
-    """Get SQLalchemy engine using credentials.
+def get_engine(host: str, port: str, db: str, username: str, password: str) -> Engine:
+    """Get sqlalchemy engine using credentials.
 
     Input:
-    db: database name
-    user: Username
     host: Hostname of the database server
     port: Port number
-    passwd: Password for the database
+    db: Database name
+    username: Username
+    password: Password for the database
     """
-    url = f'postgresql://{user}:{password}@{host}:{port}/{db}'
+    url = f'postgresql://{username}:{password}@{host}:{port}/{db}'
     engine = create_engine(url)
     Base.metadata.create_all(engine)
     return engine
