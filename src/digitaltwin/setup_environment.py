742c9371226cb235383f27a532a6dd2d2dd5aae5
Casey Li <casey.li@canterbury.ac.nz>
2023-06-07 13:31:15 +1200

    fix and update setup_environment.py functions, raising appropriate errors in right places.

--- src/digitaltwin/setup_environment.py
+++ src/digitaltwin/setup_environment.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Aug  5 17:09:13 2021.
-
-@author: pkh35, sli229
+This module provides functions for connecting to a database using SQLAlchemy.
 """
 
 import logging
@@ -10,6 +8,7 @@ import logging
 from sqlalchemy import create_engine
 from sqlalchemy.engine import Engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.exc import OperationalError
 
 from src import config
 
@@ -26,34 +25,47 @@ Base = declarative_base()
 
 
 def get_database() -> Engine:
-    """Exit the program if connection fails."""
-    engine = get_connection_from_profile()
-    log.debug("Connected to PostgreSQL database!")
-    return engine
+    """Set up the database connection. Exit the program if connection fails."""
+    try:
+        engine = get_connection_from_profile()
+        log.debug("Connected to PostgreSQL database successfully!")
+        return engine
+    except OperationalError as e:
+        raise ConnectionAbortedError("Connection to database failed. Please check .env file.") from e
 
 
 def get_connection_from_profile() -> Engine:
-    """Sets up database connection from config file."""
+    """Sets up database connection from configuration."""
     connection_keys = ["POSTGRES_HOST", "POSTGRES_PORT", "POSTGRES_DB", "POSTGRES_USER", "POSTGRES_PASSWORD"]
     host, port, db, username, password = (config.get_env_variable(key) for key in connection_keys)
 
     if any(connection_cred is None for connection_cred in [host, port, db, username, password]):
-        raise ConnectionAbortedError(f"Bad .env file. Not all f{connection_keys} set.")
+        raise ValueError("Please check the .env file as one or more of the connection credentials are missing.")
 
     return get_engine(host, port, db, username, password)
 
 
 def get_engine(host: str, port: str, db: str, username: str, password: str) -> Engine:
-    """Get sqlalchemy engine using credentials.
+    """Get SQLAlchemy engine using credentials.
 
-    Input:
-    host: Hostname of the database server
-    port: Port number
-    db: Database name
-    username: Username
-    password: Password for the database
+    Parameters
+    ----------
+    host : str
+        Hostname of the database server.
+    port : str
+        Port number.
+    db : str
+        Database name.
+    username : str
+        Username.
+    password : str
+        Password for the database.
     """
     url = f'postgresql://{username}:{password}@{host}:{port}/{db}'
     engine = create_engine(url)
     Base.metadata.create_all(engine)
     return engine
+
+
+if __name__ == "__main__":
+    get_database()
