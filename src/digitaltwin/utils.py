32aa72ee6723ad483a17059ad312ffd29e19a3fb
Casey Li <casey.li@canterbury.ac.nz>
2023-06-16 23:33:06 +1200

    updated utils.py with docstrings

--- src/digitaltwin/utils.py
+++ src/digitaltwin/utils.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-@Description:
+@Date: 16/06/2023
 @Author: sli229
 """
 
@@ -9,19 +9,42 @@ from shapely.geometry import Polygon
 
 
 def get_catchment_area(catchment_area: gpd.GeoDataFrame, to_crs: int = 2193) -> gpd.GeoDataFrame:
-    catchment_area = catchment_area.to_crs(to_crs)
-    return catchment_area
+    """
+    Convert the coordinate reference system (CRS) of the catchment area GeoDataFrame to the specified CRS.
+
+    Parameters
+    ----------
+    catchment_area : gpd.GeoDataFrame
+        The GeoDataFrame representing the catchment area.
+    to_crs : int, optional
+        Coordinate Reference System (CRS) code to convert the catchment area to. Default is 2193.
+
+    Returns
+    -------
+    gpd.GeoDataFrame
+        The catchment area GeoDataFrame with the transformed CRS.
+    """
+    return catchment_area.to_crs(to_crs)
 
 
 def get_catchment_area_polygon(catchment_area: gpd.GeoDataFrame, to_crs: int = 2193) -> Polygon:
     """
-    Extract shapely geometry polygon from the catchment boundary in the given crs
+    Retrieve the polygon representing the catchment area.
 
     Parameters
     ----------
-    catchment_area
-        Catchment area GeoPandas data frame.
-    to_crs: int = 2193
-        Specify the output's coordinate reference system. Default is 2193.
+    catchment_area : gpd.GeoDataFrame
+        The GeoDataFrame representing the catchment area.
+    to_crs : int, optional
+        Coordinate Reference System (CRS) code to convert the catchment area to. Default is 2193.
+
+    Returns
+    -------
+    Polygon
+        The polygon from the catchment area GeoDataFrame, converted to the specified CRS.
     """
-    return catchment_area.to_crs(to_crs)["geometry"][0]
+    # Convert the catchment area to the specified CRS
+    catchment_area_crs = catchment_area.to_crs(to_crs)
+    # Extract the first polygon from the geometry column
+    catchment_polygon = catchment_area_crs["geometry"].iloc[0]
+    return catchment_polygon
