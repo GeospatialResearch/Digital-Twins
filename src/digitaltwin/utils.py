4f693d797542989f8972133059106e1b60a47f67
Casey Li <casey.li@canterbury.ac.nz>
2023-06-20 17:27:11 +1200

    added docstring to get_nz_boundary_polygon()

--- src/digitaltwin/utils.py
+++ src/digitaltwin/utils.py
@@ -55,11 +55,30 @@ def get_catchment_area_polygon(catchment_area: gpd.GeoDataFrame, to_crs: int = 2
 
 
 def get_nz_boundary_polygon(engine: Engine, to_crs: int = 2193) -> Polygon:
+    """
+    Get the boundary polygon of New Zealand.
+
+    Parameters
+    ----------
+    engine : Engine
+        Engine used to connect to the database.
+    to_crs : int, optional
+        Coordinate Reference System (CRS) code to convert the boundary polygon to. Default is 2193.
+
+    Returns
+    -------
+    Polygon
+        The boundary polygon of New Zealand in the specified CRS.
+    """
+    # Query the region_geometry table from the database using the provided engine
     query = "SELECT * FROM region_geometry;"
-    regional_council = gpd.GeoDataFrame.from_postgis(query, engine, geom_col="geometry")
-    nz_boundary = regional_council.dissolve(aggfunc="sum").explode(index_parts=True).reset_index(level=0, drop=True)
+    region_geometry = gpd.GeoDataFrame.from_postgis(query, engine, geom_col="geometry")
+    # Dissolve and explode the geometries
+    nz_boundary = region_geometry.dissolve(aggfunc="sum").explode(index_parts=True).reset_index(level=0, drop=True)
+    # Calculate the area of each geometry and sort them in descending order
     nz_boundary["geometry_area"] = nz_boundary["geometry"].area
     nz_boundary = nz_boundary.sort_values(by="geometry_area", ascending=False).head(1)
+    # Convert to the desired coordinate reference system (CRS) and get the Polygon
     nz_boundary = nz_boundary.to_crs(to_crs)
     nz_boundary_polygon = nz_boundary["geometry"][0]
     return nz_boundary_polygon
