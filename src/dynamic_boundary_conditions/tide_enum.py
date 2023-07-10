169c9697e2ade7c6173bbd7b3fb53434e82b4095
Casey Li <casey.li@canterbury.ac.nz>
2023-07-10 12:04:08 +1200

    update tide_enum.py

--- src/dynamic_boundary_conditions/tide_enum.py
+++ src/dynamic_boundary_conditions/tide_enum.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-@Description: Enum(s) used in the dynamic_boundary_conditions tide module.
+@Description: Enum(s) used in the tide_slr module.
 @Author: sli229
 """
 
@@ -9,6 +9,8 @@ from enum import StrEnum
 
 class DatumType(StrEnum):
     """
+    Enum class representing different datum types.
+
     Attributes
     ----------
     LAT : str
@@ -21,5 +23,15 @@ class DatumType(StrEnum):
 
 
 class ApproachType(StrEnum):
+    """
+    Enum class representing different types of approaches.
+
+    Attributes
+    ----------
+    KING_TIDE : str
+        King Tide approach.
+    PERIOD_TIDE : str
+        Period Tide approach.
+    """
     KING_TIDE = "king_tide"
     PERIOD_TIDE = "period_tide"
