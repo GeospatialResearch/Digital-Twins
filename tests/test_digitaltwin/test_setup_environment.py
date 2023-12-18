8be78e886869433c8da665eb86e896ffc14d5d4b
Casey Li <casey.li@canterbury.ac.nz>
2023-12-18 15:39:07 +1300

    modify hierarchy of tests

--- tests/test_digitaltwin/test_setup_environment.py
+++ tests/test_digitaltwin/test_setup_environment.py
@@ -13,12 +13,12 @@ class SetupEnvironmentTest(unittest.TestCase):
         engine = setup_environment.get_connection_from_profile()
         connection = engine.connect()
         self.assertFalse(connection.closed,
-                         "The connection to the database failed")  # Check that the connection is open
+                         msg="The connection to the database failed")  # Check that the connection is open
 
     @unittest.skip("Skipping until we work on https://github.com/GeospatialResearch/Digital-Twins/issues/23")
     def test_incorrect_password(self):
         """Ensure that when a bad password is given to the database, the connection fails and an exception is raised"""
-        incorrect_password_config_path = 'tests/test_setup_environment/mock_db_configuration.yml'
+        incorrect_password_config_path = 'tests/test_digitaltwin/data/mock_db_configuration.yml'
         with self.assertRaises(OperationalError,
                                msg="get_connection_from_profile should raise an OperationalError if the password supplied is incorrect"):
             setup_environment.get_connection_from_profile(
