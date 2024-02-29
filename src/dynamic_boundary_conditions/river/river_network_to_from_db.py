be8f73cd71d400b6d2b1a7b0a72eb3cd0e07a29f
Casey Li <casey.li@canterbury.ac.nz>
2024-03-01 10:09:03 +1300

    river module use s3 bucket

--- src/dynamic_boundary_conditions/river/river_network_to_from_db.py
+++ src/dynamic_boundary_conditions/river/river_network_to_from_db.py
@@ -20,6 +20,7 @@ from sqlalchemy import select, func
 from sqlalchemy.engine import Engine
 
 from src.config import get_env_variable
+from src.digitaltwin.s3_connection import S3Manager
 from src.digitaltwin.tables import (
     check_table_exists,
     create_table,
@@ -109,32 +110,36 @@ def get_new_network_output_paths() -> Tuple[pathlib.Path, pathlib.Path]:
     """
     # Get the current timestamp in "YYYY_MM_DD_HH_MM_SS" format
     dt_string = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
-    # Get the data directory from the environment variable
-    data_dir = get_env_variable("DATA_DIR", cast_to=pathlib.Path)
-    # Define the directory for storing the REC Network and its associated data
-    network_dir = data_dir / "rec_network" / dt_string
-    # Create the REC Network directory if it does not already exist
-    network_dir.mkdir(parents=True, exist_ok=True)
-    # Create the file path for the REC Network with the current timestamp
-    network_path = (network_dir / f"{dt_string}_network.pickle")
-    # Create the file path for the REC Network data with the current timestamp
-    network_data_path = (network_dir / f"{dt_string}_network_data.geojson")
+    # Retrieve the value of the environment variable "USE_AWS_S3_BUCKET"
+    use_aws_s3_bucket = get_env_variable("USE_AWS_S3_BUCKET", cast_to=bool)
+
+    if use_aws_s3_bucket is True:
+        # Define the directory for storing the REC Network and its associated data
+        network_dir = pathlib.Path(f"stored_data/rec_network/{dt_string}")
+        # Create the file path for the REC Network with the current timestamp
+        network_path = network_dir / f"{dt_string}_network.pickle"
+        # Create the file path for the REC Network data with the current timestamp
+        network_data_path = network_dir / f"{dt_string}_network_data.geojson"
+    else:
+        # Get the data directory from the environment variable
+        data_dir = get_env_variable("DATA_DIR", cast_to=pathlib.Path)
+        # Define the directory for storing the REC Network and its associated data
+        network_dir = data_dir / "rec_network" / dt_string
+        # Create the REC Network directory if it does not already exist
+        network_dir.mkdir(parents=True, exist_ok=True)
+        # Create the file path for the REC Network with the current timestamp
+        network_path = (network_dir / f"{dt_string}_network.pickle")
+        # Create the file path for the REC Network data with the current timestamp
+        network_data_path = (network_dir / f"{dt_string}_network_data.geojson")
     return network_path, network_data_path
 
 
-def get_network_output_metadata(
-        network_path: pathlib.Path,
-        network_data_path: pathlib.Path,
-        catchment_area: gpd.GeoDataFrame) -> Tuple[str, str, str]:
+def get_network_output_metadata(catchment_area: gpd.GeoDataFrame) -> Tuple[str, str, str]:
     """
     Get metadata associated with the REC Network.
 
     Parameters
     ----------
-    network_path : pathlib.Path
-        The path to the REC Network file.
-    network_data_path : pathlib.Path
-        The path to the REC Network data file.
     catchment_area : gpd.GeoDataFrame
         A GeoDataFrame representing the catchment area.
 
@@ -144,9 +149,11 @@ def get_network_output_metadata(
         A tuple containing the absolute path to the REC Network file as a string, the absolute path to the REC Network
         data file as a string, and the Well-Known Text (WKT) representation of the catchment area's geometry.
     """
-    # Get the absolute path of the REC Network file as a string
+    # Generate timestamped file paths for REC river network and associated data storage
+    network_path, network_data_path = get_new_network_output_paths()
+    # Get the path of the REC Network file as a string
     network_path = network_path.as_posix()
-    # Get the absolute path of the REC Network data file as a string
+    # Get the path of the REC Network data file as a string
     network_data_path = network_data_path.as_posix()
     # Get the WKT representation of the catchment area's geometry
     catchment_geom = catchment_area["geometry"].to_wkt().iloc[0]
@@ -182,24 +189,29 @@ def store_rec_network_to_db(
         This function does not return any value.
     """
     log.info("Adding REC river network metadata to the database.")
-    # Get new file paths for storing both the REC Network and its associated data
-    network_path, network_data_path = get_new_network_output_paths()
-    # Save the REC river network to the specified file
-    with open(network_path, "wb") as file:
-        pickle.dump(rec_network, file)
+    # Get metadata related to the REC Network Output
+    network_path, network_data_path, geometry = get_network_output_metadata(catchment_area)
 
     # Convert the 'first_coord' and 'last_coord' columns to string format to facilitate exporting
     network_data = rec_network_data.copy()
     network_data["first_coord"] = network_data["first_coord"].astype(str)
     network_data["last_coord"] = network_data["last_coord"].astype(str)
-    # Save the REC river network data to the specified file
-    network_data.to_file(str(network_data_path), driver="GeoJSON")
+    # Retrieve the value of the environment variable "USE_AWS_S3_BUCKET"
+    use_aws_s3_bucket = get_env_variable("USE_AWS_S3_BUCKET", cast_to=bool)
+    # If True, store REC river network and its associated data in S3 bucket
+    if use_aws_s3_bucket is True:
+        S3Manager().store_object(network_path, rec_network)
+        S3Manager().store_object(network_data_path, network_data)
+    # If False, store REC river network and its associated data locally
+    else:
+        # Save the REC river network to the specified file
+        with open(network_path, "wb") as file:
+            pickle.dump(rec_network, file)
+        # Save the REC river network data to the specified file
+        network_data.to_file(network_data_path, driver="GeoJSON")
 
     # Create the REC Network table in the database if it doesn't exist
     create_table(engine, RiverNetwork)
-    # Get metadata related to the REC Network Output
-    network_path, network_data_path, geometry = get_network_output_metadata(
-        network_path, network_data_path, catchment_area)
     # Create a new query object representing the REC Network metadata
     query = RiverNetwork(
         rec_network_id=rec_network_id,
@@ -284,11 +296,20 @@ def get_existing_network(engine: Engine, existing_network_meta: gpd.GeoDataFrame
         # Log a warning message indicating the reason and IDs of the excluded REC river segments
         log.warning(f"Excluded REC from river network because '{exclusion_cause}': "
                     f"{', '.join(map(str, excluded_ids))}")
-    # Load the REC river network graph
-    with open(existing_network_series["network_path"], "rb") as file:
-        rec_network = pickle.load(file)
-    # Load the REC river network data containing geometry information
-    rec_network_data = gpd.read_file(existing_network_series["network_data_path"])
+
+    # Retrieve the value of the environment variable "USE_AWS_S3_BUCKET"
+    use_aws_s3_bucket = get_env_variable("USE_AWS_S3_BUCKET", cast_to=bool)
+    # If True, retrieve REC river network and its associated data from S3 bucket
+    if use_aws_s3_bucket is True:
+        rec_network = S3Manager().retrieve_object(existing_network_series["network_path"])
+        rec_network_data = S3Manager().retrieve_object(existing_network_series["network_data_path"])
+    # If False, retrieve REC river network and its associated data from local storage
+    else:
+        # Load the REC river network graph
+        with open(existing_network_series["network_path"], "rb") as file:
+            rec_network = pickle.load(file)
+        # Load the REC river network data containing geometry information
+        rec_network_data = gpd.read_file(existing_network_series["network_data_path"])
     # Set the data type of the 'first_coord' and 'last_coord' columns to geometry
     rec_network_data["first_coord"] = rec_network_data["first_coord"].apply(shapely.wkt.loads).astype("geometry")
     rec_network_data["last_coord"] = rec_network_data["last_coord"].apply(shapely.wkt.loads).astype("geometry")
