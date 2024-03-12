a76a7f6c4fc067ab6f30136651efc6658f9f2b87
Casey Li <casey.li@canterbury.ac.nz>
2024-03-12 15:12:31 +1300

    update S3Manager class

--- src/digitaltwin/s3_connection.py
+++ src/digitaltwin/s3_connection.py
@@ -13,12 +13,16 @@ from src import config
 
 class S3Manager:
     """
-    A class to manage connections to AWS S3 bucket.
+    A class for managing interactions with an Amazon Simple Storage Service (Amazon S3) bucket.
+    Provides methods to interact with an AWS S3 bucket, including storing and retrieving objects,
+    listing objects, removing objects, uploading files, and clearing the entire bucket.
     """
 
     def __init__(self):
         """
-        Initializes the S3Manager with AWS credentials and bucket name.
+        Initialize an S3Manager instance.
+        Sets up the S3Manager with the necessary AWS credentials obtained from environment variables and creates
+        a boto3 session and S3 client for interacting with an Amazon Simple Storage Service (Amazon S3) bucket.
         """
         self.access_key_id = config.get_env_variable("AWS_ACCESS_KEY_ID")
         self.secret_access_key = config.get_env_variable("AWS_SECRET_ACCESS_KEY")
@@ -28,16 +32,16 @@ class S3Manager:
 
     def _create_session(self):
         """
-        Creates and returns a boto3 session with the provided AWS credentials.
+        Create a boto3 session using the provided AWS credentials.
         """
         session = boto3.Session(
             aws_access_key_id=self.access_key_id,
             aws_secret_access_key=self.secret_access_key)
         return session
 
-    def list_object_keys(self):
+    def list_objects(self):
         """
-        Lists objects in the AWS S3 bucket.
+        List objects in the S3 bucket.
         """
         resp = self.s3.list_objects_v2(Bucket=self.bucket_name)
         object_keys = []
@@ -47,6 +51,9 @@ class S3Manager:
         return object_keys
 
     def store_object(self, s3_object_key, data):
+        """
+        Store an object in the S3 bucket.
+        """
         if isinstance(s3_object_key, pathlib.Path):
             s3_object_key = s3_object_key.as_posix()
         if isinstance(data, nx.DiGraph):
@@ -56,6 +63,9 @@ class S3Manager:
         self.s3.put_object(Bucket=self.bucket_name, Key=s3_object_key, Body=body)
 
     def retrieve_object(self, s3_object_key):
+        """
+        Retrieve an object from the S3 bucket.
+        """
         if isinstance(s3_object_key, pathlib.Path):
             s3_object_key = s3_object_key.as_posix()
         resp = self.s3.get_object(Bucket=self.bucket_name, Key=s3_object_key)
@@ -73,16 +83,22 @@ class S3Manager:
         return data
 
     def remove_object(self, s3_object_key):
+        """
+        Remove an object from the S3 bucket.
+        """
         self.s3.delete_object(Bucket=self.bucket_name, Key=s3_object_key)
 
-    def store_file(self, s3_object_key, file_name):
+    def store_file(self, s3_object_key, file_path):
+        """
+        Upload a file to the S3 bucket.
+        """
         if isinstance(s3_object_key, pathlib.Path):
             s3_object_key = s3_object_key.as_posix()
-        self.s3.upload_file(Bucket=self.bucket_name, Key=s3_object_key, Filename=file_name)
+        self.s3.upload_file(Bucket=self.bucket_name, Key=s3_object_key, Filename=file_path)
 
     def clear_bucket(self):
         """
-        Clear objects in the AWS S3 bucket.
+        Clear the entire S3 bucket by removing all objects.
         """
         bucket = self.s3.Bucket(self.bucket_name)
         bucket.objects.all().delete()
