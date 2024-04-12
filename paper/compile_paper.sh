e79cf69cb9e99a1710f5804f914e6f32d4620d34
Luke Parkinson <lukelpparkinson@gmail.com>
2024-04-12 13:26:18 +1200

    Remove previous pdf when generating a new one

--- paper/compile_paper.sh
+++ paper/compile_paper.sh
@@ -1,6 +1,8 @@
 # Get the root directory of this repository (the parent dir of this script)
 PAPER_DIR="$(dirname "$(readlink -fm "$0")")"
 
+rm -f "$PAPER_DIR"/paper.pdf
+
 docker run --rm \
     --volume "$PAPER_DIR":/data \
     --user "$(id -u)":"$(id -g)" \
