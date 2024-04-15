683928c0cfa135e5b25e1e423aaf8d016e2f7d5e
Casey Li <casey.li@canterbury.ac.nz>
2024-04-15 16:41:58 +1200

    fix and update s3_connection.py

--- src/digitaltwin/s3_connection.py
+++ src/digitaltwin/s3_connection.py
@@ -2,8 +2,10 @@ import logging
 import pathlib
 import pickle
 from io import BytesIO
+from typing import List, Union
 
 import boto3
+import botocore.exceptions
 import geopandas as gpd
 import networkx as nx
 import xarray as xr
@@ -21,7 +23,7 @@ class S3Manager:
     listing objects, removing objects, uploading files, and clearing the entire bucket.
     """
 
-    def __init__(self):
+    def __init__(self) -> None:
         """
         Initialize an S3Manager instance.
         Sets up the S3Manager with the necessary AWS credentials obtained from environment variables and creates
@@ -31,9 +33,10 @@ class S3Manager:
         self.secret_access_key = config.get_env_variable("AWS_SECRET_ACCESS_KEY")
         self.bucket_name = config.get_env_variable("AWS_BUCKET_NAME")
         self.session = self._create_session()
-        self.s3 = self.session.client("s3")
+        self.s3_client = self.session.client("s3")
+        self.s3_resource = self.session.resource("s3")
 
-    def _create_session(self):
+    def _create_session(self) -> boto3.session.Session:
         """
         Create a boto3 session using the provided AWS credentials.
         """
@@ -43,12 +46,12 @@ class S3Manager:
             aws_secret_access_key=self.secret_access_key)
         return session
 
-    def list_objects(self):
+    def list_objects(self) -> List[str]:
         """
         List objects in the S3 bucket.
         """
         # Retrieve a list of objects from the S3 bucket
-        resp = self.s3.list_objects_v2(Bucket=self.bucket_name)
+        resp = self.s3_client.list_objects_v2(Bucket=self.bucket_name)
         # Initialize an empty list to store the object keys
         object_keys = []
         # Check if the response contains any objects
@@ -59,7 +62,7 @@ class S3Manager:
                 object_keys.append(obj["Key"])
         return object_keys
 
-    def store_object(self, s3_object_key, data):
+    def store_object(self, s3_object_key: Union[str, pathlib.Path], data) -> None:
         """
         Store an object in the S3 bucket.
         """
@@ -75,11 +78,11 @@ class S3Manager:
             # Convert the data to a JSON string
             body = data.to_json(drop_id=True)
         # Upload the data to the S3 bucket using the provided object key
-        self.s3.put_object(Bucket=self.bucket_name, Key=s3_object_key, Body=body)
+        self.s3_client.put_object(Bucket=self.bucket_name, Key=s3_object_key, Body=body)
         # Log a message confirming successful storage in the S3 bucket
         log.info(f"Successfully stored `{s3_object_key}` in the S3 bucket.")
 
-    def retrieve_object(self, s3_object_key):
+    def retrieve_object(self, s3_object_key: Union[str, pathlib.Path]):
         """
         Retrieve an object from the S3 bucket.
         """
@@ -88,7 +91,7 @@ class S3Manager:
             # Convert the pathlib.Path object to a string representation
             s3_object_key = s3_object_key.as_posix()
         # Retrieve the object from the S3 bucket using the provided object key
-        resp = self.s3.get_object(Bucket=self.bucket_name, Key=s3_object_key)
+        resp = self.s3_client.get_object(Bucket=self.bucket_name, Key=s3_object_key)
         # Read the content of the retrieved object
         body = resp["Body"].read()
         # Determine the appropriate action based on the file extension of the object
@@ -116,16 +119,20 @@ class S3Manager:
         log.info(f"Successfully retrieved '{s3_object_key}' from the S3 bucket.")
         return data
 
-    def remove_object(self, s3_object_key):
+    def remove_object(self, s3_object_key: Union[str, pathlib.Path]) -> None:
         """
         Remove an object from the S3 bucket.
         """
+        # Check if the provided s3_object_key is a pathlib.Path object
+        if isinstance(s3_object_key, pathlib.Path):
+            # Convert the pathlib.Path object to a string representation
+            s3_object_key = s3_object_key.as_posix()
         # Delete the object with the provided object key from the S3 bucket
-        self.s3.delete_object(Bucket=self.bucket_name, Key=s3_object_key)
+        self.s3_client.delete_object(Bucket=self.bucket_name, Key=s3_object_key)
         # Log a message confirming successful deletion from the S3 bucket
         log.info(f"Successfully deleted '{s3_object_key}' from the S3 bucket.")
 
-    def store_file(self, s3_object_key, file_path):
+    def store_file(self, s3_object_key: Union[str, pathlib.Path], file_path: Union[str, pathlib.Path]) -> None:
         """
         Upload a file to the S3 bucket.
         """
@@ -134,17 +141,18 @@ class S3Manager:
             # Convert the pathlib.Path object to a string representation
             s3_object_key = s3_object_key.as_posix()
         # Upload the file at 'file_path' to the S3 bucket with the provided object key
-        self.s3.upload_file(Bucket=self.bucket_name, Key=s3_object_key, Filename=file_path)
+        self.s3_client.upload_file(Bucket=self.bucket_name, Key=s3_object_key, Filename=file_path.as_posix())
         # Log a message confirming successful storage in the S3 bucket
         log.info(f"Successfully stored `{s3_object_key}` in the S3 bucket.")
 
-    def clear_bucket(self):
+    def clear_bucket(self) -> None:
         """
         Clear the entire S3 bucket by removing all objects.
         """
         # Access the S3 bucket
-        bucket = self.s3.Bucket(self.bucket_name)
+        bucket = self.s3_resource.Bucket(self.bucket_name)
         # Delete all objects within the bucket
-        bucket.objects.all().delete()
+        for obj in bucket.objects.all():
+            obj.delete()
         # Log a message confirming successful removal of all objects from the S3 bucket
         log.info("Successfully removed all objects from the S3 bucket.")
