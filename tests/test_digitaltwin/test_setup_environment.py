0790325ea44360f97a0ecc9cdab52ffb4d3acc84
Luke Parkinson <luke.parkinson@canterbury.ac.nz>
2024-07-03 12:54:48 +1200

    Add environment variable for whether to skip database integration tests or run them

--- tests/test_digitaltwin/test_setup_environment.py
+++ tests/test_digitaltwin/test_setup_environment.py
@@ -1,34 +1,44 @@
 import os
-import platform
 import unittest
 
 import pytest
 from sqlalchemy.exc import OperationalError
 
+from src import config
 from src.digitaltwin import setup_environment
 
 
 class SetupEnvironmentTest(unittest.TestCase):
 
-    @pytest.mark.skipif(
-        platform.system() != "Windows",
-        reason="This test only runs on local dev machines with a database running. "
-               "It is not set up to integrate into a test suite.")
+    @classmethod
+    def setUpClass(cls):
+        """Set up arguments used for testing."""
+        # flag for checking if tests requiring database to be active should run
+        cls.run_database_integration_tests = config.get_env_variable("TEST_DATABASE_INTEGRATION",
+                                                                     default=True,
+                                                                     allow_empty=True,
+                                                                     cast_to=bool
+                                                                     )
+        cls.DATABASE_SKIP_REASON = """TEST_DATABASE_INTEGRATION env var is not True, so the test is skipped.
+            This is intentional at all times on the GitHub Actions."""
+
     def test_connection(self):
         """Check a connection to the database can be made with the default parameters of get_connection_from_profile"""
+        # Skip this if database tests are not intended to be run in this environment
+        if not self.run_database_integration_tests:
+            pytest.skip(self.DATABASE_SKIP_REASON)
         engine = setup_environment.get_connection_from_profile()
         connection = engine.connect()
         self.assertFalse(connection.closed,
                          msg="The connection to the database failed and is needed for these tests.")
 
-    @pytest.mark.skipif(
-        platform.system() != "Windows",
-        reason="This test only runs on local dev machines with a database running. "
-               "It is not set up to integrate into a test suite.")
     def test_incorrect_password(self):
         """Ensure that when a bad password is given to the database, the connection fails and an exception is raised"""
-        # Override the variables supplied by the .env file with an incorrect password
+        # Skip this if database tests are not intended to be run in this environment
+        if not self.run_database_integration_tests:
+            pytest.skip(self.DATABASE_SKIP_REASON)
         self.test_connection()  # This test case requires an active connection to trust the result.
+        # Override the variables supplied by the .env file with an incorrect password
         os.environ["POSTGRES_PASSWORD"] = "incorrect_password"
         with self.assertRaises(OperationalError,
                                msg="get_connection_from_profile should raise an OperationalError"
