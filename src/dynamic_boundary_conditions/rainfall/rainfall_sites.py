abd516b81b9784956be2965b5d83b91348d619e5
Casey Li <casey.li@canterbury.ac.nz>
2024-07-22 11:48:49 +1200

    update get_rainfall_sites_data() to remove unnecessary header info

--- src/dynamic_boundary_conditions/rainfall/rainfall_sites.py
+++ src/dynamic_boundary_conditions/rainfall/rainfall_sites.py
@@ -3,10 +3,9 @@
 
 import logging
 
-import requests
-from requests.structures import CaseInsensitiveDict
-import pandas as pd
 import geopandas as gpd
+import pandas as pd
+import requests
 from sqlalchemy.engine import Engine
 
 from src.digitaltwin import tables
@@ -24,20 +23,9 @@ def get_rainfall_sites_data() -> str:
         The rainfall sites data as a string.
     """
     url = "https://api.niwa.co.nz/hirds/sites"
-    headers = CaseInsensitiveDict()
-    headers["Accept"] = "application/json, text/plain, */*"
-    headers["Accept-Language"] = "en-GB,en-US;q=0.9,en;q=0.8"
-    headers["Connection"] = "keep-alive"
-    headers["Origin"] = "https://hirds.niwa.co.nz"
-    headers["Referer"] = "https://hirds.niwa.co.nz/"
-    headers["sec-ch-ua"] = '"" Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96""'
-    headers["sec-ch-ua-mobile"] = "?0"
-    headers["sec-ch-ua-platform"] = "Windows"
-    headers["Sec-Fetch-Dest"] = "empty"
-    headers["Sec-Fetch-Mode"] = "cors"
-    headers["Sec-Fetch-Site"] = "same-site"
-    headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)\
-        Chrome/96.0.4664.45 Safari/537.36"
+    headers = {
+        "Referer": "https://hirds.niwa.co.nz/"
+    }
     # Send HTTP GET request to the specified URL with headers
     response = requests.get(url, headers=headers)
     # Return the response content as a text string
