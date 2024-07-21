66a75b879ba7e6e47e94ed91172bafc7113e0926
Casey Li <casey.li@canterbury.ac.nz>
2024-07-22 11:53:44 +1200

    update get_site_url_key() to remove unnecessary header info

--- src/dynamic_boundary_conditions/rainfall/rainfall_data_from_hirds.py
+++ src/dynamic_boundary_conditions/rainfall/rainfall_data_from_hirds.py
@@ -2,12 +2,11 @@
 """Fetch rainfall data from the HIRDS website."""
 
 import re
-from typing import List, NamedTuple, Optional
 from io import StringIO
+from typing import List, NamedTuple, Optional
 
-import requests
-from requests.structures import CaseInsensitiveDict
 import pandas as pd
+import requests
 
 
 def get_site_url_key(site_id: str, idf: bool) -> str:
@@ -27,21 +26,9 @@ def get_site_url_key(site_id: str, idf: bool) -> str:
         Unique URL key of the requested rainfall site.
     """
     url = "https://api.niwa.co.nz/hirds/report"
-    headers = CaseInsensitiveDict()
-    headers["Accept"] = "application/json, text/plain, */*"
-    headers["Accept-Language"] = "en-GB,en-US;q=0.9,en;q=0.8"
-    headers["Connection"] = "keep-alive"
-    headers["Content-Type"] = "application/json"
-    headers["Origin"] = "https://hirds.niwa.co.nz"
-    headers["Referer"] = "https://hirds.niwa.co.nz/"
-    headers["Sec-Fetch-Dest"] = "empty"
-    headers["Sec-Fetch-Mode"] = "cors"
-    headers["Sec-Fetch-Site"] = "same-site"
-    headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)\
-        Chrome/96.0.4664.110 Safari/537.36"
-    headers["sec-ch-ua"] = '"" Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96""'
-    headers["sec-ch-ua-mobile"] = "?0"
-    headers["sec-ch-ua-platform"] = '""Windows""'
+    headers = {
+        "Referer": "https://hirds.niwa.co.nz/"
+    }
     # Convert idf parameter to lowercase string
     idf = str(idf).lower()
     data = f'{{"site_id":"{site_id}","idf":{idf}}}'
