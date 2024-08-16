4cc5ca5c275e09a3addd1478a724d13938dcb4dd
Luke Parkinson <luke.parkinson@canterbury.ac.nz>
2024-08-16 13:25:20 +1200

    Add eslint-ignore to rule that causes incorrect placement of "."

--- visualisation/src/pages/AboutPage.vue
+++ visualisation/src/pages/AboutPage.vue
@@ -10,12 +10,14 @@
     <h4>Source code</h4>
     <p>
       The source code for the app <a href="https://github.com/GeospatialResearch/Digital-Twins">can be obtained from
-      GitHub here</a>.
+        GitHub here</a>.
     </p>
     <hr>
     <h4>App licence</h4>
     <p>
       The code license for this particular version of the code can be found on our
+      <!-- Disabling eslint rule so that "." can show directly after link anchor. -->
+      <!-- eslint-disable vue/singleline-html-element-content-newline -->
       <router-link :to="{ name: 'License'}">License Page</router-link>.
     </p>
   </b-container>
