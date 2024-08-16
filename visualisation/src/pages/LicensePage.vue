3ca0d34ebdab3c3e878680274596ef7982a708cd
Luke Parkinson <41398636+LukeParky@users.noreply.github.com>
2024-08-16 14:49:07 +1200

    Dynamically import LICENSE into vue website. (#227)
    * Dynamically import LICENSE into vue website.
    
    To allow this I had to allow the docker build context to access the root directory and so had to expand the .dockerignore.
    
    * Remove empty <style> to appease sonar cloud
    
    * Increase warnings to errors on eslint
    
    * Add eslint-ignore to rule that causes incorrect placement of "."

---
+++ visualisation/src/pages/LicensePage.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- Page displaying About information. Context of the web app, license, source code, etc.  -->
+  <div class="multiline">
+    {{ license }}
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import titleMixin from "@/mixins/title"
+import license from "../../../LICENSE"
+
+export default Vue.extend({
+  name: "LicensePage",
+  title: "License",
+  mixins: [titleMixin],
+  data() {
+    return {
+      license: license
+    }
+  },
+});
+</script>
+
+<style scoped>
+div.multiline {
+  white-space: pre;
+}
+</style>
