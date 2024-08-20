85e5c2ba7f5602b53c81b73bd9feaab1691730a5
Luke Parkinson <luke.parkinson@canterbury.ac.nz>
2024-08-21 10:29:33 +1200

    Add full docstrings to child pollution_tables.py child classes

--- src/pollution_model/pollution_tables.py
+++ src/pollution_model/pollution_tables.py
@@ -19,24 +19,20 @@ class _BaseMedusa2ModelOutput(Base):
 
     Attributes
     ----------
-    __tablename__ : str
-       Name of the database table.
     scenario_id : int
-       The rainfall event scenario ID this feature was processed with, makes up part of the primary key.
-    spatial_feature_id: int
-       The id of the spatial feature this row is associated with, makes up part of the primary key.
+        The rainfall event scenario ID this feature was processed with, makes up part of the primary key.
     surface_type : str
-       The type of surface this is. Converted to string from Enum.
+        The type of surface this is. Converted to string from Enum.
     total_suspended_solids : float
-       Total suspended solids of the surface.
+        Total suspended solids of the surface.
     total_copper : float
-       Total copper of the surface.
+        Total copper of the surface.
     total_zinc: float
-       Total zinc of the surface.
+        Total zinc of the surface.
     dissolved_copper float
-       Total dissolved copper of the surface.
+        Total dissolved copper of the surface.
     dissolved_zinc: float
-       Total dissolved zinc of the surface.
+        Total dissolved zinc of the surface.
     """
 
     __abstract__ = True  # This is an abstract base class
@@ -44,23 +40,33 @@ class _BaseMedusa2ModelOutput(Base):
     @property
     @abc.abstractmethod
     def __tablename__(self) -> str:
-        """This must be overridden in the child class. e.g. __tablename__ = "medusa2_model_output_buildings."""
-        raise NotImplementedError("__tablename__ must be overidden in the child class."
-                                  "e.g. __tablename__ = \"medusa2_model_output_buildings.\"")
+        """
+        Name of the database table.
+        This must be overridden in the child class. e.g. __tablename__ = "medusa2_model_output_buildings."
+        """
+        raise NotImplementedError("""__tablename__ must be overridden in the child class.
+                                  e.g. __tablename__ = "medusa2_model_output_buildings". """)
+
+    @property
+    @abc.abstractmethod
+    def geometry_table(self) -> str:
+        """
+        Name of the spatial feature table this table is associated with.
+        This must be overridden in the child class. e.g. geometry_table = "nz_buildings".
+        """
+        raise NotImplementedError("""geometry_table must be overridden in the child class.
+                                  e.g. geometry_table = "nz_buildings". """)
 
     @property
     @abc.abstractmethod
     def spatial_feature_id(self) -> Column:
-        """This must be overridden in the child class. e.g. __tablename__ = "medusa2_model_output_buildings"."""
-        raise NotImplementedError("spatial_feature_id must be overridden in the child class."
-                                  "Eg. spatial_feature_id = Column(Integer, primary_key=True, name=\"building_id\")")
-
-    @property
-    @abc.abstractmethod
-    def geometry_table(self) -> Column:
-        """This must be overridden in the child class. e.g. geometry_table = "nz_buildings"."""
-        raise NotImplementedError("geometry_table must be overridden in the child class."
-                                  "Eg. geometry_table = \"nz_buildings\"")
+        """
+        The road_id this row is associated with, makes up part of the primary key.
+        This must be overridden in the child class.
+        e.g. spatial_feature_id = Column(Integer, primary_key=True, name="building_id").
+        """
+        raise NotImplementedError("""spatial_feature_id must be overridden in the child class.
+                                  eg. spatial_feature_id = Column(Integer, primary_key=True, name="building_id"). """)
 
     scenario_id = Column(Integer, primary_key=True)
     surface_type = Column(String, nullable=False)
@@ -72,16 +78,66 @@ class _BaseMedusa2ModelOutput(Base):
 
 
 class Medusa2ModelOutputBuildings(_BaseMedusa2ModelOutput):
-    """Class representing the medusa2_model_output_buildings table."""  # pylint: disable=too-few-public-methods
+    """
+    Class representing the medusa2_model_output_buildings table.
+
+    Attributes
+    ----------
+    __tablename__ : str
+       Name of the database table.
+    geometry_table : str
+        Name of the spatial feature table this table is associated with (nz_building_outlines).
+    spatial_feature_id: int
+        The building_id this row is associated with, makes up part of the primary key.
+    scenario_id : int
+        The rainfall event scenario ID this feature was processed with, makes up part of the primary key.
+    surface_type : str
+        The type of surface this is. Converted to string from Enum.
+    total_suspended_solids : float
+        Total suspended solids of the surface.
+    total_copper : float
+        Total copper of the surface.
+    total_zinc: float
+        Total zinc of the surface.
+    dissolved_copper float
+        Total dissolved copper of the surface.
+    dissolved_zinc: float
+        Total dissolved zinc of the surface.
+    """  # pylint: disable=too-few-public-methods
 
     __tablename__ = "medusa2_model_output_buildings"
-    spatial_feature_id = Column(Integer, primary_key=True, name="building_id")
     geometry_table = "nz_building_outlines"
+    spatial_feature_id = Column(Integer, primary_key=True, name="building_id")
 
 
 class Medusa2ModelOutputRoads(_BaseMedusa2ModelOutput):
-    """Class representing the medusa2_model_output_roads table."""  # pylint: disable=too-few-public-methods
+    """
+    Class representing the medusa2_model_output_roads table.
+
+    Attributes
+    ----------
+    __tablename__ : str
+       Name of the database table.
+    geometry_table : str
+        Name of the spatial feature table this table is associated with (nz_roads).
+    spatial_feature_id: int
+        The road_id this row is associated with, makes up part of the primary key.
+    scenario_id : int
+        The rainfall event scenario ID this feature was processed with, makes up part of the primary key.
+    surface_type : str
+        The type of surface this is. Converted to string from Enum.
+    total_suspended_solids : float
+        Total suspended solids of the surface.
+    total_copper : float
+        Total copper of the surface.
+    total_zinc: float
+        Total zinc of the surface.
+    dissolved_copper float
+        Total dissolved copper of the surface.
+    dissolved_zinc: float
+        Total dissolved zinc of the surface.
+    """  # pylint: disable=too-few-public-methods
 
     __tablename__ = "medusa2_model_output_roads"
-    spatial_feature_id = Column(Integer, primary_key=True, name="road_id")
     geometry_table = "nz_roads"
+    spatial_feature_id = Column(Integer, primary_key=True, name="road_id")
