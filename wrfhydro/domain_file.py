4dc3a75cd3b5f8d191099f8aa7c7cc4a6f7939ab
Martin Nguyen <martin.nguyen@canterbury.ac.nz>
2025-06-30 11:32:01 +1200

    Add initial domain script

---
+++ wrfhydro/domain_file.py
@@ -0,0 +1,63 @@
+def write_wps_namelist(
+    max_dom,
+    e_we, e_sn,
+    ref_lat, ref_lon,
+    dx, dy,
+    geog_data_path,
+    save_file_to
+):
+    content = f"""
+&share
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!  Specify the number of domains
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+ max_dom = {max_dom},
+
+/
+
+&geogrid
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! Define the extend in west-east (e_we) and south-north (e_sn) directions
+!  Note: will create a domain of size (e_we-1) x (e_sn-1)
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+ e_we              =  {e_we},
+ e_sn              =  {e_sn},
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! Define the center point of your domain
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+ ref_lat   =  {ref_lat}
+ ref_lon   =  {ref_lon}
+ 
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! Define the domain grid spacing (in meters)
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+ dx = {dx},
+ dy = {dy},
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! Define the map projection
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+ map_proj = 'lambert',
+ truelat1  = {ref_lat},
+ truelat2  = {ref_lat},
+ stand_lon = {ref_lon},
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! Define the data sources and data path
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+ geog_data_res     = 'default',
+ geog_data_path = {geog_data_path}
+
+/
+    """
+    with open(save_file_to, "w") as file:
+        file.write(content)
