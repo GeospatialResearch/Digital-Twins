a2dcd02d41be2705815a1a6b73af7fd8991723a8
Luke Parkinson <luke.parkinson@canterbury.ac.nz>
2025-08-01 11:53:59 +1200

    log discovered plugins

--- src/eddie/discover_plugins.py
+++ src/eddie/discover_plugins.py
@@ -1,10 +1,17 @@
 import importlib
+import logging
 import pkgutil
 from types import ModuleType
 
+from eddie.digitaltwin.utils import setup_logging
+
+setup_logging()
+log = logging.getLogger(__name__)
 
 def discover_plugins() -> dict[str, ModuleType]:
-    return {
+    plugins = {
         name: importlib.import_module(name)
-        for finder, name, _is_pkg in pkgutil.iter_modules() if name.startswith('eddie_')
+        for _finder, name, _is_pkg in pkgutil.iter_modules() if name.startswith('eddie_')
     }
+    log.info(f"Plugins discovered: {plugins}")
+    return plugins
