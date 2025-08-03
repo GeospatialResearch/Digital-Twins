53373f0f0dbbc4dfb8c08f0145babc947ecf9be1
Luke Parkinson <luke.parkinson@canterbury.ac.nz>
2025-08-04 09:53:27 +1200

    Create main datastore more easily log discovered plugins

--- src/geoserver/__init__.py
+++ src/geoserver/__init__.py
@@ -2,7 +2,7 @@
 Functions and utilities for loading and serving data with geoserver.
 Imports here are accessible directly by `from src import geoserver`.
 """
-from .database_layers import create_datastore_layer, create_db_store_if_not_exists
+from .database_layers import create_datastore_layer, create_db_store_if_not_exists, create_main_db_store
 from .geoserver_common import create_workspace_if_not_exists
 from .raster_layers import add_gtiff_to_geoserver, create_viridis_style_if_not_exists
 
@@ -10,6 +10,7 @@ __all__ = [
     "add_gtiff_to_geoserver",
     "create_datastore_layer",
     "create_db_store_if_not_exists",
+    "create_main_db_store",
     "create_viridis_style_if_not_exists",
     "create_workspace_if_not_exists",
 ]
