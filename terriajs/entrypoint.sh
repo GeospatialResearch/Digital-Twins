faa920fd92ad7085923435b8cc38a1ec5db4dd1a
Luke Parkinson <luke.parkinson@canterbury.ac.nz>
2025-10-22 12:52:51 +1300

    Fix sonarqube cloud maintenance warnings

--- terriajs/entrypoint.sh
+++ terriajs/entrypoint.sh
@@ -24,7 +24,7 @@ ENV_VARS_TO_FILL='$CESIUM_ACCESS_TOKEN,$LINZ_BASEMAPS_API_KEY,$BACKEND_URL,$GEOS
 # Substitute variables and save whole file to variable, sponge is not available to buffer.
 for FILE_TO_SUB in "wwwroot/config.json" "wwwroot/init/catalog.json"
 do
-  SUBBED=`envsubst "$ENV_VARS_TO_FILL" < "$FILE_TO_SUB"`
+  SUBBED=$(envsubst "$ENV_VARS_TO_FILL" < "$FILE_TO_SUB")
   # Overwrite original file.
   echo "$SUBBED" > "$FILE_TO_SUB"
 done
