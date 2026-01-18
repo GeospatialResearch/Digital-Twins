154ddc1b5f386f8b77e2f5a5a1498ae1be763bb2
Luke Parkinson <luke.parkinson@canterbury.ac.nz>
2026-01-19 10:17:17 +1300

    TEMP2: saving - something about adding styles etc.

--- src/eddie/geoserver/__init__.py
+++ src/eddie/geoserver/__init__.py
@@ -20,7 +20,7 @@ Functions and utilities for loading and serving data with geoserver.
 Imports here are accessible directly by `from eddie import geoserver`.
 """
 from .database_layers import create_datastore_layer, create_db_store_if_not_exists, create_main_db_store
-from .geoserver_common import create_workspace_if_not_exists
+from .geoserver_common import create_workspace_if_not_exists, style_exists
 from .terria_catalogs import get_terria_catalog, Workspaces
 from .raster_layers import add_gtiff_to_geoserver, add_style, create_viridis_style_if_not_exists
 
@@ -34,4 +34,5 @@ __all__ = [
     "create_workspace_if_not_exists",
     "get_terria_catalog",
     "Workspaces",
+    "style_exists"
 ]
--- src/eddie/geoserver/geoserver_common.py
+++ src/eddie/geoserver/geoserver_common.py
@@ -113,3 +113,41 @@ def style_exists(style_name: str) -> bool:
     # If it does not meet the expected results then raise an error
     # Raise error manually, so we can configure the text
     raise requests.HTTPError(response.text, response=response)
+
+
+def add_style(style_name: str, style_xml: bytes) -> None:
+    """#todo"""
+    log.info(f"Creating style '{style_name}.sld' if it does not exist.")
+    if geoserver.style_exists(style_name):
+        log.debug(f"Style '{style_name}.sld' already exists.")
+    else:
+        # Create the style base
+        create_style_data = f"""
+           <style>
+               <name>{style_name}</name>
+               <filename>{style_name}.sld</filename>
+           </style>
+           """
+        create_style_response = requests.post(
+            f'{geoserver.get_geoserver_url()}/styles',
+            data=create_style_data,
+            headers=_xml_header,
+            auth=(EnvVariable.GEOSERVER_ADMIN_NAME, EnvVariable.GEOSERVER_ADMIN_PASSWORD)
+        )
+        create_style_response.raise_for_status()
+    # PUT the style definition .sld file into the style base
+    sld_response = requests.put(
+        f'{ggeoserver.get_geoserver_url()}/styles/{style_name}',
+        data=style_xml,
+        headers={"Content-type": "application/vnd.ogc.sld+xml"},
+        auth=(EnvVariable.GEOSERVER_ADMIN_NAME, EnvVariable.GEOSERVER_ADMIN_PASSWORD)
+    )
+    sld_response.raise_for_status()
+    log.info(f"Style '{style_name}.sld' created.")
+
+
+def create_viridis_style_if_not_exists() -> None:
+    """Create a GeoServer style for flood rasters using the viridis color scale."""
+    style_name = "viridis_raster"
+    with open('./src/eddie_floodresilience/flood_model/templates/viridis_raster.sld', 'rb') as payload:
+        add_style(style_name, payload)
